98584b59d17fdfc2a1514fa00fb8f11327279c23
Darius Foo <darius.foo.tw@gmail.com>
2014-06-17 19:19:56 +0800

    Menu items

--- src/Demo.java
+++ src/Demo.java
@@ -1,9 +1,16 @@
+import javafx.beans.InvalidationListener;
 import javafx.beans.property.ReadOnlyStringWrapper;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -12,7 +19,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
@@ -130,191 +142,199 @@ public class Demo extends Application {
 
 	private VBox createColumn() {
 		VBox col = new VBox();
-		col.setPadding(new Insets(15, 12, 15, 12));
-		col.setSpacing(10);
+//		col.setPadding(new Insets(15, 12, 15, 12));
+//		col.setSpacing(10);
 		col.setPrefWidth(400);
 		HBox.setHgrow(col, Priority.ALWAYS);
 
-		col.setOnDragOver((DragEvent event) -> {
-			if (event.getGestureSource() != col
-					&& event.getDragboard().hasString()) {
-				event.acceptTransferModes(TransferMode.COPY);
-			}
-
-			event.consume();
-		});
-
 		col.setStyle(STYLE_BORDERS);
-
-		col.setOnDragEntered((DragEvent event) -> {
-			if (event.getGestureSource() != col
-					&& event.getDragboard().hasString()) {
-
-				if (done)
-					return;
-
-				col.setStyle(STYLE_YELLOW_BORDERS);
-			}
-
-			event.consume();
-		});
-
-		col.setOnDragExited((DragEvent event) -> {
-			col.setStyle(STYLE_BORDERS);
-
-			event.consume();
-		});
-
-		col.setOnDragDropped((DragEvent event) -> {
-			Dragboard db = event.getDragboard();
-			boolean success = false;
-			if (db.hasString() && !done) {
-				done = true;
-				success = true;
-
-				// add the item here
-				DragData dd = DragData.deserialize(db.getString());
-
-				if (dd.source == DragSource.PANEL_MILESTONES) {
-					BorderPane item = items.get(dd.index);
-					col.getChildren().add(item);
-				} else if (dd.source == DragSource.TREE_ISSUES) {
-					BorderPane item = createItem("issue: " + dd.text);
-					col.getChildren().add(item);
-				} else if (dd.source == DragSource.TREE_MILESTONES) {
-					col.getChildren().clear();
-
-					col.setPadding(new Insets(15, 12, 15, 12));
-					Label which = new Label();
-					which.setText("Milestone " + dd.text);
-
-					which.setFont(Font.font("System Regular", FontWeight.BOLD,
-							16));
-
-					Label issuesLabel = new Label();
-					issuesLabel.setText("Issues");
-
-					ObservableList<String> data = FXCollections
-							.observableArrayList();
-					// data.addAll("one", "two", "three");
-
-					ListView<String> issues = new ListView<>(data);
-					String defaultIssuesStyle = issues.getStyle();
-
-					issues.setOnDragOver((e) -> {
-						if (event.getGestureSource() != issues
-								&& event.getDragboard().hasString()) {
-							event.acceptTransferModes(TransferMode.COPY);
-						}
-						event.consume();
-					});
-
-					issues.setOnDragEntered((e) -> {
-						if (event.getGestureSource() != issues
-								&& event.getDragboard().hasString()) {
-							Dragboard db2 = event.getDragboard();
-							if (db2.hasString()) {
-								DragData dd2 = DragData.deserialize(db
-										.getString());
-								if (dd2.source == DragSource.TREE_ISSUES) {
-									issues.setStyle(STYLE_YELLOW_BORDERS);
-								}
-							}
-						}
-						event.consume();
-					});
-
-					issues.setOnDragExited((e) -> {
-						issues.setStyle(defaultIssuesStyle);
-
-						event.consume();
-					});
-
-					issues.setOnDragDropped((e) -> {
-						Dragboard db2 = event.getDragboard();
-						boolean success2 = false;
-						if (db2.hasString()) {
-							success2 = true;
-							DragData dd2 = DragData.deserialize(db2.getString());
-							System.out.println("oiukjjkjl");
-							if (dd2.source == DragSource.TREE_ISSUES) {
-								data.add(dd2.text);
-								System.out.println("aklsjdkasjl");
-							}
-						}
-						event.setDropCompleted(success2);
-
-						event.consume();
-					});
-
-					// // Creating tree items
-					// final TreeItem<String> childNode1 = new TreeItem<>(
-					// "Child Node 1");
-					// final TreeItem<String> childNode2 = new TreeItem<>(
-					// "Child Node 2");
-					// final TreeItem<String> childNode3 = new TreeItem<>(
-					// "Child Node 3");
-					//
-					// // Creating the root element
-					// final TreeItem<String> root = new
-					// TreeItem<>("Root node");
-					// root.setExpanded(true);
-					//
-					// // Adding tree items to the root
-					// root.getChildren().add(childNode1);
-					// root.getChildren().add(childNode2);
-					// root.getChildren().add(childNode3);
-					//
-					// // Creating a column
-					// TreeTableColumn<String, String> column = new
-					// TreeTableColumn<>(
-					// "Issue");
-					// column.setPrefWidth(150);
-					//
-					// TreeTableColumn<String, String> length = new
-					// TreeTableColumn<>(
-					// "Length");
-					// length.setPrefWidth(150);
-					//
-					// // Defining cell content
-					// column.setCellValueFactory((
-					// CellDataFeatures<String, String> p) -> new
-					// ReadOnlyStringWrapper(
-					// p.getValue().getValue()));
-					// length.setCellValueFactory((
-					// CellDataFeatures<String, String> p) -> new
-					// ReadOnlyStringWrapper(
-					// Integer.toString(p.getValue().getValue().length())));
-					//
-					// // Creating a tree table view
-					// final TreeTableView<String> issues = new TreeTableView<>(
-					// root);
-					// issues.getColumns().add(column);
-					// issues.getColumns().add(length);
-					// issues.setPrefWidth(152);
-					// issues.setShowRoot(true);
-
-					col.getChildren().add(which);
-					col.getChildren().add(issuesLabel);
-					col.getChildren().add(issues);
-
-				} else if (dd.source == DragSource.TREE_LABELS) {
-					BorderPane item = createItem("label: " + dd.text);
-					col.getChildren().add(item);
-				} else if (dd.source == DragSource.TREE_CONTRIBUTORS) {
-					BorderPane item = createItem("contributor: " + dd.text);
-					col.getChildren().clear();
-					col.getChildren().add(item);
-				}
-			}
-
-			// this removes the item from its previous parent
-			event.setDropCompleted(success);
-
-			event.consume();
-		});
-
 		col.setAlignment(Pos.TOP_CENTER);
+		
+		ListView<String> listView = new ListView<>();
+		ObservableList<String> items = FXCollections.observableArrayList();
+		items.add("asakda");
+		items.add("kljkljhsk");
+		listView.setItems(items);
+		
+		col.getChildren().add(listView);
+		
+
+//		col.setOnDragOver((DragEvent event) -> {
+//			if (event.getGestureSource() != col
+//					&& event.getDragboard().hasString()) {
+//				event.acceptTransferModes(TransferMode.COPY);
+//			}
+//
+//			event.consume();
+//		});
+//
+//		col.setOnDragEntered((DragEvent event) -> {
+//			if (event.getGestureSource() != col
+//					&& event.getDragboard().hasString()) {
+//
+//				if (done)
+//					return;
+//
+//				col.setStyle(STYLE_YELLOW_BORDERS);
+//			}
+//
+//			event.consume();
+//		});
+//
+//		col.setOnDragExited((DragEvent event) -> {
+//			col.setStyle(STYLE_BORDERS);
+//
+//			event.consume();
+//		});
+//
+//		col.setOnDragDropped((DragEvent event) -> {
+//			Dragboard db = event.getDragboard();
+//			boolean success = false;
+//			if (db.hasString() && !done) {
+//				done = true;
+//				success = true;
+//
+//				// add the item here
+//				DragData dd = DragData.deserialize(db.getString());
+//
+//				if (dd.source == DragSource.PANEL_MILESTONES) {
+//					BorderPane item = items.get(dd.index);
+//					col.getChildren().add(item);
+//				} else if (dd.source == DragSource.TREE_ISSUES) {
+//					BorderPane item = createItem("issue: " + dd.text);
+//					col.getChildren().add(item);
+//				} else if (dd.source == DragSource.TREE_MILESTONES) {
+//					col.getChildren().clear();
+//
+//					col.setPadding(new Insets(15, 12, 15, 12));
+//					Label which = new Label();
+//					which.setText("Milestone " + dd.text);
+//
+//					which.setFont(Font.font("System Regular", FontWeight.BOLD,
+//							16));
+//
+//					Label issuesLabel = new Label();
+//					issuesLabel.setText("Issues");
+//
+//					ObservableList<String> data = FXCollections
+//							.observableArrayList();
+//					// data.addAll("one", "two", "three");
+//
+//					ListView<String> issues = new ListView<>(data);
+//					String defaultIssuesStyle = issues.getStyle();
+//
+//					issues.setOnDragOver((e) -> {
+//						if (event.getGestureSource() != issues
+//								&& event.getDragboard().hasString()) {
+//							event.acceptTransferModes(TransferMode.COPY);
+//						}
+//						event.consume();
+//					});
+//
+//					issues.setOnDragEntered((e) -> {
+//						if (event.getGestureSource() != issues
+//								&& event.getDragboard().hasString()) {
+//							Dragboard db2 = event.getDragboard();
+//							if (db2.hasString()) {
+//								DragData dd2 = DragData.deserialize(db
+//										.getString());
+//								if (dd2.source == DragSource.TREE_ISSUES) {
+//									issues.setStyle(STYLE_YELLOW_BORDERS);
+//								}
+//							}
+//						}
+//						event.consume();
+//					});
+//
+//					issues.setOnDragExited((e) -> {
+//						issues.setStyle(defaultIssuesStyle);
+//
+//						event.consume();
+//					});
+//
+//					issues.setOnDragDropped((e) -> {
+//						Dragboard db2 = event.getDragboard();
+//						boolean success2 = false;
+//						if (db2.hasString()) {
+//							success2 = true;
+//							DragData dd2 = DragData.deserialize(db2.getString());
+//							System.out.println("oiukjjkjl");
+//							if (dd2.source == DragSource.TREE_ISSUES) {
+//								data.add(dd2.text);
+//								System.out.println("aklsjdkasjl");
+//							}
+//						}
+//						event.setDropCompleted(success2);
+//
+//						event.consume();
+//					});
+//
+//					// // Creating tree items
+//					// final TreeItem<String> childNode1 = new TreeItem<>(
+//					// "Child Node 1");
+//					// final TreeItem<String> childNode2 = new TreeItem<>(
+//					// "Child Node 2");
+//					// final TreeItem<String> childNode3 = new TreeItem<>(
+//					// "Child Node 3");
+//					//
+//					// // Creating the root element
+//					// final TreeItem<String> root = new
+//					// TreeItem<>("Root node");
+//					// root.setExpanded(true);
+//					//
+//					// // Adding tree items to the root
+//					// root.getChildren().add(childNode1);
+//					// root.getChildren().add(childNode2);
+//					// root.getChildren().add(childNode3);
+//					//
+//					// // Creating a column
+//					// TreeTableColumn<String, String> column = new
+//					// TreeTableColumn<>(
+//					// "Issue");
+//					// column.setPrefWidth(150);
+//					//
+//					// TreeTableColumn<String, String> length = new
+//					// TreeTableColumn<>(
+//					// "Length");
+//					// length.setPrefWidth(150);
+//					//
+//					// // Defining cell content
+//					// column.setCellValueFactory((
+//					// CellDataFeatures<String, String> p) -> new
+//					// ReadOnlyStringWrapper(
+//					// p.getValue().getValue()));
+//					// length.setCellValueFactory((
+//					// CellDataFeatures<String, String> p) -> new
+//					// ReadOnlyStringWrapper(
+//					// Integer.toString(p.getValue().getValue().length())));
+//					//
+//					// // Creating a tree table view
+//					// final TreeTableView<String> issues = new TreeTableView<>(
+//					// root);
+//					// issues.getColumns().add(column);
+//					// issues.getColumns().add(length);
+//					// issues.setPrefWidth(152);
+//					// issues.setShowRoot(true);
+//
+//					col.getChildren().add(which);
+//					col.getChildren().add(issuesLabel);
+//					col.getChildren().add(issues);
+//
+//				} else if (dd.source == DragSource.TREE_LABELS) {
+//					BorderPane item = createItem("label: " + dd.text);
+//					col.getChildren().add(item);
+//				} else if (dd.source == DragSource.TREE_CONTRIBUTORS) {
+//					BorderPane item = createItem("contributor: " + dd.text);
+//					col.getChildren().clear();
+//					col.getChildren().add(item);
+//				}
+//			}
+//
+//			// this removes the item from its previous parent
+//			event.setDropCompleted(success);
+//
+//			event.consume();
+//		});
 
 		return col;
 	}
@@ -441,34 +461,74 @@ public class Demo extends Application {
 	}
 
 	private Parent createRoot() {
+		MenuBar menuBar = createMenuBar();
+		
 		BorderPane root = new BorderPane();
 
 		columns = new HBox();
-		columns.setPadding(new Insets(15, 12, 15, 12));
-		columns.setSpacing(10);
+//		columns.setPadding(new Insets(15, 12, 15, 12));
+//		columns.setSpacing(10);
 
 		VBox col1 = createColumn();
+		VBox col2 = createColumn();
+		VBox col3 = createColumn();
+		// VBox col3 = createColumn();
 		// col1.getChildren().addAll(createItem(), createItem());
 
-		VBox col2 = createColumn();
-		// VBox col3 = createColumn();
+		columns.getChildren().addAll(col1, col2 , col3);
 
-		columns.getChildren().addAll(col1, col2);// , col3);
+//		SplitPane splitPane = new SplitPane();
+//		splitPane.getItems().addAll(createTreeView(), columns);
+//		splitPane.setDividerPositions(0.2);
 
-		SplitPane splitPane = new SplitPane();
-		splitPane.getItems().addAll(createTreeView(), columns);
-		splitPane.setDividerPositions(0.2);
-
-		root.setCenter(splitPane);
+		root.setCenter(columns);
+		root.setTop(menuBar);
 
 		return root;
+	}
+
+	private MenuBar createMenuBar() {
+		MenuBar menuBar = new MenuBar();
+
+		Menu projects = new Menu("Projects");
+		MenuItem config = new MenuItem("Configuration");
+		projects.getItems().addAll(config);
+		
+		Menu milestones = new Menu("Milestones");
+		MenuItem newMilestone = new MenuItem("New Milestone");
+		milestones.getItems().addAll(newMilestone);
+		
+		Menu issues = new Menu("Issues");
+		MenuItem newIssue = new MenuItem("New Issue");
+		issues.getItems().addAll(newIssue);
+		
+		Menu labels = new Menu("Labels");
+		MenuItem newLabel = new MenuItem("New Label");
+		labels.getItems().addAll(newLabel);
+
+		Menu view = new Menu("View");
+		Menu columns = new Menu("Change number of columns....");
+		view.getItems().addAll(columns);
+
+		final ToggleGroup numberOfCols = new ToggleGroup();
+		for (int i=2; i<=9; i++) {
+		    RadioMenuItem item = new RadioMenuItem(Integer.toString(i));
+		    item.setUserData(i);
+		    item.setToggleGroup(numberOfCols);
+		    columns.getItems().add(item);
+		    
+		    if (i == 3) item.setSelected(true);
+		}
+		
+		menuBar.getMenus().addAll(projects, milestones, issues, labels, view);
+		return menuBar;
 	}
 
 	@Override
 	public void start(Stage stage) {
 		Scene scene = new Scene(createRoot(), 800, 600);
 		setUpHotkeys(scene);
-
+	
 		mainStage = stage;
 		stage.setTitle("Demo");
 		stage.setMinWidth(800);
