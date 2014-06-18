a1a7c8f4b38156d2e669d715e5da110c662ff2c1
Darius Foo <darius.foo.tw@gmail.com>
2014-06-18 16:56:31 +0800

    TurboIssue now uses ObservableLists internally

--- src/logic/TurboIssue.java
+++ src/logic/TurboIssue.java
@@ -1,42 +1,42 @@
 package logic;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import org.eclipse.egit.github.core.Issue;
 import org.eclipse.egit.github.core.Label;
 
 public class TurboIssue {
 	private Issue ghIssue;
-//	private String title;
-	private String description;
-	private int id;
-	private ArrayList<TurboLabel> labels;
+	private ObservableList<TurboLabel> labels;
 	private TurboContributor assignee;
 	private TurboMilestone milestone;
 	
-	
-	
 	public TurboIssue(String title, String desc) {
 		setTitle(title);
-		this.description = desc;
+		setDescription(desc);
 	}
 	
 	public TurboIssue(Issue issue) {
 		this.ghIssue = issue;
+		
 		setTitle(issue.getTitle());
-		this.description = issue.getBody();
-		this.id = issue.getNumber();
+		setDescription(issue.getBody());
+		setId(issue.getNumber());
+		
 		this.assignee = new TurboContributor(issue.getAssignee());
 		this.milestone = new TurboMilestone(issue.getMilestone());
 		this.labels = getLabels(issue);
 	}
 	
-	private ArrayList<TurboLabel> getLabels(Issue issue) {
-		ArrayList<TurboLabel> turboLabels = new ArrayList<TurboLabel>();
+	private ObservableList<TurboLabel> getLabels(Issue issue) {
+		ObservableList<TurboLabel> turboLabels = FXCollections.observableArrayList();
 		List<Label> labels = issue.getLabels();
 		for (Label label : labels) {
 			turboLabels.add(new TurboLabel(label));
@@ -53,31 +53,25 @@ public class TurboIssue {
 		return ghIssue;
 	}
 	
-//	public String getTitle() {
-//		return title;
-//	}
-//	public void setTitle(String title) {
-//		this.title = title;
-//	}
-	
     private StringProperty title = new SimpleStringProperty();
     public final String getTitle() {return title.get();}
     public final void setTitle(String value) {title.set(value);}
     public StringProperty titleProperty() {return title;}
 	
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public int getId() {
-		return id;
-	}
-	public ArrayList<TurboLabel> getLabels() {
+    private StringProperty description = new SimpleStringProperty();
+    public final String getDescription() {return description.get();}
+    public final void setDescription(String value) {description.set(value);}
+    public StringProperty descriptionProperty() {return description;}
+
+    private IntegerProperty id = new SimpleIntegerProperty();
+    public final int getId() {return id.get();}
+    private final void setId(int value) {id.set(value);}
+    public IntegerProperty idProperty() {return id;}
+    
+	public ObservableList<TurboLabel> getLabels() {
 		return labels;
 	}
-	public void setLabels(ArrayList<TurboLabel> labels) {
+	public void setLabels(ObservableList<TurboLabel> labels) {
 		this.labels = labels;
 	}
 	public TurboContributor getAssignee() {
