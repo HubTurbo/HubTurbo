634a51a044a1004e64d482a30a712f8b16e36d5d
Darius Foo <darius.foo.tw@gmail.com>
2014-06-20 22:36:27 +0800

    Implemented multipleSelection properly for FilterableCBL

--- src/ui/FilterableCheckboxList.java
+++ src/ui/FilterableCheckboxList.java
@@ -9,6 +9,7 @@ import org.controlsfx.control.CheckListView;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.geometry.Insets;
@@ -28,6 +29,19 @@ public class FilterableCheckboxList {
 	FilteredList<String> objects;
 
 	CompletableFuture<List<Integer>> response;
+	
+	// State variables
+	
+	int previouslyChecked = -1;
+	int previousState = 0;
+	
+	// Disables the callback to prevent an infinite loop (since
+	// updating the check state updates the check state)
+	boolean disabled = false;
+	
+	// Also disables the callback. Throttles its activation
+	// to once per change
+	boolean oneActivation = false;
 
 	public FilterableCheckboxList(Stage parentStage,
 			ObservableList<Listable> objects) {
@@ -52,31 +66,84 @@ public class FilterableCheckboxList {
 		searchField.setPromptText("Search");
 
 		CheckListView<String> checkListView = new CheckListView<>(objects);
-		checkListView.getSelectionModel().setSelectionMode(
-				multipleSelection ? SelectionMode.MULTIPLE
-						: SelectionMode.SINGLE);
+		checkListView.getSelectionModel()
+				.setSelectionMode(SelectionMode.SINGLE);
 
 		initialCheckedState.forEach((i) -> checkListView.getCheckModel()
 				.select(i));
 
-		// checkListView.getSelectionModel().getSelectedItems()
-		// .addListener(new ListChangeListener<Listable>() {
-		// @Override
-		// public void onChanged(
-		// ListChangeListener.Change<? extends Listable> c) {
-		// // updateText(selectedItemsLabel, c.getList());
-		// System.out.println("selection changed");
-		// }
-		// });
-		// checkListView.getCheckModel().getSelectedItems()
-		// .addListener(new ListChangeListener<Listable>() {
-		// @Override
-		// public void onChanged(
-		// ListChangeListener.Change<? extends Listable> c) {
-		//
-		// System.out.println("check state changed");
-		// }
-		// });
+		if (!multipleSelection) {
+			assert initialCheckedState.size() == 1
+					|| initialCheckedState.size() == 0;
+			if (initialCheckedState.size() == 1) {
+				previousState = 1;
+				previouslyChecked = initialCheckedState.get(0);
+			}
+		}
+		
+		// getSelectionModel().getSelectedItems() can also have a listener
+
+		/**
+		 * A small state machine to get around API oddities.
+		 * 
+		 * The goal is to implement the `multipleSelection` flag, which when
+		 * false allows only a single checkbox to be checked.
+		 * 
+		 * Basically there are 4 states which a given pair of checkboxes can be in:
+		 * 00, 01, 10, 11. This is exhaustive because we only need a second checked box
+		 * to reduce the state back to 01 or 10. State 11 technically doesn't exist
+		 * because we transition as soon as we enter it. Transitions: 1 bit at a time.
+		 * 
+		 * previousState and previouslyChecked are used to track this information.
+		 * c.wasAdded() || c.wasRemoved(), disabled, and oneActivation are for throttling
+		 * the execution of the callback in various ways.
+		 */
+		
+		checkListView.getCheckModel().getSelectedItems()
+				.addListener(new ListChangeListener<String>() {
+					@Override
+					public void onChanged(
+							ListChangeListener.Change<? extends String> c) {
+
+						while (c.next()) {
+							if (!multipleSelection
+									&& (c.wasAdded() || c.wasRemoved())
+									&& !disabled && !oneActivation) {
+								List<Integer> currentlyChecked = checkListView
+										.getCheckModel().getSelectedIndices();
+								oneActivation = true;
+
+								if (currentlyChecked.size() == 1) {
+									assert previousState != 1;
+									previouslyChecked = currentlyChecked.get(0);
+									previousState = 1;
+								} else if (currentlyChecked.size() == 2) {
+									assert previousState == 1;
+									assert previouslyChecked != -1;
+
+									// There is no state 2: it is always skipped
+									previousState = 1;
+
+									int newlyChecked = previouslyChecked == currentlyChecked
+											.get(0) ? currentlyChecked.get(1)
+											: currentlyChecked.get(0);
+
+									previouslyChecked = newlyChecked;
+									disabled = true;
+									checkListView.getCheckModel()
+											.clearAndSelect(newlyChecked);
+									disabled = false;
+								} else {
+									assert currentlyChecked.size() == 0;
+									assert previousState == 1;
+									previousState = 0;
+									previouslyChecked = -1;
+								}
+							}
+						}
+						oneActivation = false;
+					}
+				});
 
 		Stage stage = new Stage();
 
