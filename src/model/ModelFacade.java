86c2e54efaddfd786df29d8120ed2e224bb03b3b
jwwong <jwwong.91@gmail.com>
2014-06-24 11:47:25 +0800

    Removed setRepository() from ModelFacade

--- src/model/ModelFacade.java
+++ src/model/ModelFacade.java
@@ -1,13 +1,10 @@
 package model;
 
-import org.eclipse.egit.github.core.IRepositoryIdProvider;
-import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.client.GitHubClient;
 
 public class ModelFacade {
 	
 	private GitHubClient client = new GitHubClient();
-	private IRepositoryIdProvider repoId = null;
 	private IssueManager issueManager = new IssueManager();
 	private AuthenticationManager authManager = new AuthenticationManager(client);
 	private MilestoneManager milestoneManager = new MilestoneManager();
@@ -16,14 +13,6 @@ public class ModelFacade {
 	
 	public boolean login(String userId, String password) {
 		return authManager.login(userId, password);
-	}
-	
-	public void setRepository(String owner, String repository) {
-		if (owner != null) {
-			repoId = RepositoryId.create(owner, repository);
-		} else {
-			repoId = RepositoryId.create(client.getUser(), repository);
-		}
 	}
 	
 	public IssueManager getIssueManager() {
