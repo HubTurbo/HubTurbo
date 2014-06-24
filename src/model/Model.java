a1ea777153f9513bb1a7758b3ce6cc4d6f0b7270
jwwong <jwwong.91@gmail.com>
2014-06-24 14:34:23 +0800

    Refactored application level data loading

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,7 +1,21 @@
 package model;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.eclipse.egit.github.core.IRepositoryIdProvider;
+import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.Label;
+import org.eclipse.egit.github.core.Milestone;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.CollaboratorService;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.LabelService;
+import org.eclipse.egit.github.core.service.MilestoneService;
 
 public class Model {
 	
@@ -9,17 +23,29 @@ public class Model {
 	public static final String MILESTONES_OPEN = "open";
 	public static final String MILESTONES_CLOSED = "closed";
 	
+	private CollaboratorService collabService;
+	private IssueService issueService;
+	private LabelService labelService;
+	private MilestoneService milestoneService;
+
 	private List<TurboCollaborator> collaborators = new ArrayList<TurboCollaborator>();
 	private List<TurboIssue> issues = new ArrayList<TurboIssue>();
 	private List<TurboLabel> labels = new ArrayList<TurboLabel>();
-	private List<TurboMilestone> milestones = new ArrayList<TurboMilestone>(); 
-
-	public List<TurboIssue> getIssues() {
-		return this.issues;
+	private List<TurboMilestone> milestones = new ArrayList<TurboMilestone>();
+	
+	public Model(GitHubClient ghClient) {
+		this.collabService = new CollaboratorService(ghClient);
+		this.issueService = new IssueService(ghClient);
+		this.labelService = new LabelService(ghClient);
+		this.milestoneService = new MilestoneService(ghClient);
 	}
 
 	public List<TurboCollaborator> getCollaborators() {
 		return collaborators;
+	}
+	
+	public List<TurboIssue> getIssues() {
+		return this.issues;
 	}
 
 	public List<TurboLabel> getLabels() {
@@ -29,4 +55,65 @@ public class Model {
 	public List<TurboMilestone> getMilestones() {
 		return milestones;
 	}
+	
+	public boolean loadCollaborators(IRepositoryIdProvider repoId) {
+		try {
+			List<User> ghCollaborators = collabService.getCollaborators(repoId);
+			for(User ghCollaborator : ghCollaborators) {
+				collaborators.add(new TurboCollaborator(ghCollaborator));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean loadIssues(IRepositoryIdProvider repoId) {
+		Map<String, String> filters = new HashMap<String, String>();
+		filters.put(IssueService.FIELD_FILTER, "all");
+		filters.put(IssueService.FILTER_STATE, "all");
+		try {		
+			List<Issue> ghIssues = issueService.getIssues(repoId, filters);
+			for (Issue ghIssue : ghIssues) {
+				issues.add(new TurboIssue(ghIssue));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean loadLabels(IRepositoryIdProvider repoId){
+		try {
+			List<Label> ghLabels = labelService.getLabels(repoId);
+			for (Label ghLabel : ghLabels) {
+				labels.add(new TurboLabel(ghLabel));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean loadMilestones(IRepositoryIdProvider repoId){
+		try {		
+			List<Milestone> ghMilestones = milestoneService.getMilestones(repoId, Model.MILESTONES_ALL);
+			for (Milestone ghMilestone : ghMilestones) {
+				milestones.add(new TurboMilestone(ghMilestone));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
