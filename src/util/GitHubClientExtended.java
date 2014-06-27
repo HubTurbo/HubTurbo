69d75e1e10d0a66177dca30c7df682c508135982
Naomi Leow <naomileowwx@gmail.com>
2014-06-28 02:14:52 +0800

    with subclass of githubclient which exposes some required methods:

---
+++ src/util/GitHubClientExtended.java
@@ -0,0 +1,29 @@
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.client.GitHubRequest;
+
+public class GitHubClientExtended extends GitHubClient{
+	public GitHubClientExtended(){
+		
+	}
+	
+	public HttpURLConnection createConnection(GitHubRequest request) throws IOException{
+		HttpURLConnection connection = createGet(request.generateUri());
+		return connection;
+	}
+	
+	public InputStream getStream(HttpURLConnection request)
+			throws IOException {
+		return super.getStream(request);
+	}
+	
+	public Object getBody(GitHubRequest request, InputStream stream)
+			throws IOException {
+		return super.getBody(request, stream);
+	}
+}
