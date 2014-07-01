2e7691ca252259163b04f0fe4d1c85354c8d03fe
Darius Foo <darius.foo.tw@gmail.com>
2014-07-01 15:39:10 +0800

    Fixed layout of filter dialog

--- src/ui/FilterDialog.java
+++ src/ui/FilterDialog.java
@@ -1,12 +1,6 @@
 package ui;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
-
-import org.controlsfx.control.textfield.TextFields;
-
 import filter.FilterExpression;
 import filter.Parser;
 import javafx.application.Platform;
@@ -15,6 +9,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -25,13 +20,13 @@ import model.Model;
 public class FilterDialog implements Dialog<FilterExpression> {
 
 	private final Stage parentStage;
-	private final Model logic;
+//	private final Model logic;
 
 	private final CompletableFuture<FilterExpression> response;
 
 	public FilterDialog(Stage parentStage, Model logic) {
 		this.parentStage = parentStage;
-		this.logic = logic;
+//		this.logic = logic;
 		
 		response = new CompletableFuture<>();
 	}
@@ -42,47 +37,54 @@ public class FilterDialog implements Dialog<FilterExpression> {
 	}
 
 	private Node createRoot(Stage stage) {
-        TextField field = new TextField("title: one");
+		
+		Label explanatory = new Label("Filter issues by writing a series of predicates.\n\ne.g. \"all issues assigned to John that aren't closed and are due in milestones v0.1 and v0.2\"\n\nassignee(john) ~status(closed) (milestone(v0.1) or milestone(v0.2)");
+		explanatory.setWrapText(true);
+		
+        TextField field = new TextField();
         HBox.setHgrow(field, Priority.ALWAYS);
-        setupAutocompletion(field);
-                 
-        Button close = new Button("Close");
+//        setupAutocompletion(field);
+
+        HBox buttonContainer = new HBox();
+        Button close = new Button("Filter");
         close.setOnAction((e) -> {
         	response.complete(Parser.parse(field.getText()));
         	stage.close();
         });
+        buttonContainer.setAlignment(Pos.CENTER_RIGHT);
+        buttonContainer.getChildren().add(close);
         
         VBox layout = new VBox();
-        layout.getChildren().addAll(field, close);
-        layout.setAlignment(Pos.CENTER_RIGHT);
+        layout.setAlignment(Pos.CENTER_LEFT);
         layout.setSpacing(10);
         HBox.setHgrow(layout, Priority.ALWAYS);
+        layout.getChildren().addAll(explanatory, field, buttonContainer);
         
         return layout;
 	}
 
-	private void setupAutocompletion(TextField field) {
-        ArrayList<String> words = new ArrayList<String>();
-        words.addAll(logic.getIssues().stream().map((x) -> x.getListName()).collect(Collectors.toList()));
-        words.addAll(logic.getLabels().stream().map((x) -> x.getListName()).collect(Collectors.toList()));
-        words.addAll(logic.getMilestones().stream().map((x) -> x.getListName()).collect(Collectors.toList()));
-        words.addAll(logic.getCollaborators().stream().map((x) -> x.getListName()).collect(Collectors.toList()));
-        addSyntax(words);
-        
-        TextFields.bindAutoCompletion(field, words.toArray());
-	}
+//	private void setupAutocompletion(TextField field) {
+//        ArrayList<String> words = new ArrayList<String>();
+//        words.addAll(logic.getIssues().stream().map((x) -> x.getListName()).collect(Collectors.toList()));
+//        words.addAll(logic.getLabels().stream().map((x) -> x.getListName()).collect(Collectors.toList()));
+//        words.addAll(logic.getMilestones().stream().map((x) -> x.getListName()).collect(Collectors.toList()));
+//        words.addAll(logic.getCollaborators().stream().map((x) -> x.getListName()).collect(Collectors.toList()));
+//        addSyntax(words);
+//        
+//        TextFields.bindAutoCompletion(field, words.toArray());
+//	}
 
-	private void addSyntax(ArrayList<String> keywords) {
-		keywords.addAll(Arrays.asList(new String[] {
-				"milestone:",
-				"labels:",
-				"assignee:",
-				"title:",
-		}));
-		for (String keyword : new ArrayList<String>(keywords)) {
-			keywords.add("-" + keyword);
-		}
-	}
+//	private void addSyntax(ArrayList<String> keywords) {
+//		keywords.addAll(Arrays.asList(new String[] {
+//				"milestone:",
+//				"labels:",
+//				"assignee:",
+//				"title:",
+//		}));
+//		for (String keyword : new ArrayList<String>(keywords)) {
+//			keywords.add("-" + keyword);
+//		}
+//	}
 
 	private void showDialog() {
 
@@ -90,7 +92,7 @@ public class FilterDialog implements Dialog<FilterExpression> {
 		layout.setPadding(new Insets(15));
 		layout.setSpacing(10);
 
-		Scene scene = new Scene(layout, 400, 100);
+		Scene scene = new Scene(layout, 530, 180);
 
 		Stage stage = new Stage();
 		stage.setTitle("Filter");
