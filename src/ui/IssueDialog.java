0281419881e2bf743d2c4220005388096ebaab0e
Darius Foo <darius.foo.tw@gmail.com>
2014-07-02 10:55:56 +0800

    Fixed crashes with GitHub button on Linux

--- src/ui/IssueDialog.java
+++ src/ui/IssueDialog.java
@@ -175,24 +175,77 @@ public class IssueDialog implements Dialog<String> {
 		toGh.setStyle("-fx-font-family: github-octicons; -fx-font-size: 20px; -fx-background-color: transparent; -fx-padding: 0 0 0 0;");
 		toGh.setText("G");
 		toGh.setOnMouseClicked((MouseEvent e) -> {
-			try {
-		        Desktop desktop = Desktop.getDesktop();
-		        if (Desktop.isDesktopSupported()
-		                && desktop.isSupported(Desktop.Action.BROWSE)) {
-		            URI uri = new URI(issue.getHtmlUrl());
-		            desktop.browse(uri);
-		        }
-		    } catch (IOException ex) {
-		        ex.printStackTrace();
-		    } catch (URISyntaxException ex) {
-		        ex.printStackTrace();
-		    }
+			browse(issue.getHtmlUrl());
 			response.complete("gh");
 			stage.close();
 		});
 
 		buttons.getChildren().addAll(toGh, ok, cancel);
 		return buttons;
+	}
+
+	private void browse(String htmlUrl) {
+
+		final String osName = System.getProperty("os.name");
+		
+		if (osName.startsWith("Mac OS") || osName.startsWith("Windows")) {
+			browseWithDesktop(htmlUrl);
+		} else {
+			// Assume *nix
+			browseUnix(htmlUrl);
+		}
+	}
+	
+	private void browseWithDesktop(String htmlUrl) {
+		try {
+			if (Desktop.isDesktopSupported()) {
+		        Desktop desktop = Desktop.getDesktop();
+		        if (desktop.isSupported(Desktop.Action.BROWSE)) {
+
+		            URI uri = new URI(htmlUrl);
+		            desktop.browse(uri);
+		        }
+	        }
+	    } catch (IOException ex) {
+	        ex.printStackTrace();
+	    } catch (URISyntaxException ex) {
+	        ex.printStackTrace();
+	    }
+	}
+
+	private void browseUnix(String url) {
+
+		final String[] UNIX_BROWSE_CMDS = new String[] {"google-chrome", "firefox", "www-browser", "opera", "konqueror", "epiphany", "mozilla", "netscape", "w3m", "lynx" };
+		for (final String cmd : UNIX_BROWSE_CMDS) {
+			
+			if (unixCommandExists(cmd)) {
+				try {
+					Runtime.getRuntime().exec(new String[] {cmd, url.toString()});
+				} catch (IOException e) {
+				}
+				return;
+			}
+		}
+	}
+
+	private static boolean unixCommandExists(final String cmd) {
+		Process whichProcess;
+		try {
+			whichProcess = Runtime.getRuntime().exec(new String[] { "which", cmd });
+			boolean finished = false;
+			do {
+				try {
+					whichProcess.waitFor();
+					finished = true;
+				} catch (InterruptedException e) {
+					return false;
+				}
+			} while (!finished);
+
+			return whichProcess.exitValue() == 0;
+		} catch (IOException e1) {
+			return false;
+		}
 	}
 
 	private Parent createParentsBox(Stage stage) {
