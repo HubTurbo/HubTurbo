68067f56c05976267e9fb2856c899442511dcda5
Naomi Leow <naomileowwx@gmail.com>
2014-07-04 11:27:05 +0800

    Use weakchangelisteners in labeldisblaybox

--- src/ui/LabelDisplayBox.java
+++ src/ui/LabelDisplayBox.java
@@ -1,12 +1,15 @@
 package ui;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.beans.value.WeakChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.WeakListChangeListener;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import model.TurboLabel;
@@ -15,6 +18,7 @@ public class LabelDisplayBox extends FlowPane {
 
 	private ObservableList<TurboLabel> labels;
 	private boolean displayWhenEmpty;
+	private ArrayList<Object> changeListeners = new ArrayList<Object>();
 	
 	public LabelDisplayBox() {
 		this(FXCollections.observableArrayList(), false);
@@ -25,6 +29,20 @@ public class LabelDisplayBox extends FlowPane {
 		this.displayWhenEmpty = displayWhenEmpty;
 		setup();
 	}
+	
+	private ListChangeListener<TurboLabel> createLabelsChangeListener(){
+		WeakReference<LabelDisplayBox> that = new WeakReference<LabelDisplayBox>(this);
+		ListChangeListener<TurboLabel> listener = new ListChangeListener<TurboLabel>() {
+			@Override
+			public void onChanged(ListChangeListener.Change<? extends TurboLabel> arg0) {
+				if(that.get() != null){
+					that.get().populateWithLabels();
+				}
+			}
+		};
+		changeListeners.add(listener);
+		return listener;
+	}
 
 	private void setup() {
 		if (displayWhenEmpty) {
@@ -33,15 +51,7 @@ public class LabelDisplayBox extends FlowPane {
 		setHgap(3);
 		setVgap(3);
 
-		WeakReference<LabelDisplayBox> that = new WeakReference<LabelDisplayBox>(this);
-		labels.addListener(new ListChangeListener<TurboLabel>() {
-			@Override
-			public void onChanged(ListChangeListener.Change<? extends TurboLabel> arg0) {
-				if(that.get() != null){
-					that.get().populateWithLabels();
-				}
-			}
-		});
+		labels.addListener(new WeakListChangeListener<TurboLabel>(createLabelsChangeListener()));
 		populateWithLabels();
 	}
 
@@ -49,6 +59,23 @@ public class LabelDisplayBox extends FlowPane {
 		this.labels = labels;
 		populateWithLabels();
 		return this;
+	}
+	
+	private ChangeListener<String> createLabelNameListener(Label labelText){
+		WeakReference<Label> labelTextRef = new WeakReference<Label>(labelText);
+		ChangeListener<String> listener = new ChangeListener<String>() {
+			@Override
+			public void changed(
+					ObservableValue<? extends String> stringProperty,
+					String oldValue, String newValue) {
+				Label labelText = labelTextRef.get();
+				if(labelText != null){
+					labelText.setText(newValue);
+				}
+			}
+		};
+		changeListeners.add(listener);
+		return listener;
 	}
 	
 	private void populateWithLabels() {
@@ -66,14 +93,7 @@ public class LabelDisplayBox extends FlowPane {
 		for (TurboLabel label : labels) {
 			Label labelText = new Label(label.getName());
 			labelText.setStyle(getStyleFor(label));
-			label.nameProperty().addListener(new ChangeListener<String>() {
-				@Override
-				public void changed(
-						ObservableValue<? extends String> stringProperty,
-						String oldValue, String newValue) {
-					labelText.setText(newValue);//TODO:
-				}
-			});
+			label.nameProperty().addListener(new WeakChangeListener<String>(createLabelNameListener(labelText)));
 			getChildren().add(labelText);
 		}
 	}
