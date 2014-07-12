ef6cf707a635753a1e4d5177d0ab13ed38d26064
Naomi Leow <naomileowwx@gmail.com>
2014-07-12 22:48:10 +0800

    added issue edit title command

---
+++ src/command/TurboIssueEditTitle.java
@@ -0,0 +1,42 @@
+package command;
+
+import java.io.IOException;
+
+import service.ServiceManager;
+import model.Model;
+import model.TurboIssue;
+
+public class TurboIssueEditTitle extends TurboIssueCommand{
+	
+	private String newTitle;
+	
+	public TurboIssueEditTitle(Model model, TurboIssue issue, String title){
+		super(model, issue);
+		this.newTitle = title;
+	}
+	
+	private void logTitleChange(String newTitle, String prevTitle){
+		String changeLog = ("Title edited: [previous: " + newTitle + "] [new: " + prevTitle + "]\n");
+		ServiceManager.getInstance().logIssueChanges(issue.getId(), changeLog);
+	}
+
+	@Override
+	public boolean execute() {
+		String previousTitle = issue.getTitle();
+		try {
+			ServiceManager.getInstance().editIssueTitle(issue.getId(), newTitle);
+			issue.setTitle(newTitle);
+			logTitleChange(newTitle, previousTitle);
+			isSuccessful = true;
+		} catch (IOException e) {
+			isSuccessful = false;
+			e.printStackTrace();
+		}
+		return isSuccessful;
+	}
+
+	@Override
+	public boolean undo() {
+		return true;
+	}
+}
