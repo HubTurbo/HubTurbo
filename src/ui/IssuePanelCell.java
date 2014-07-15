c66a17c81d4bf8617cd0958492cfff4189d5fff3
Darius Foo <darius.foo.tw@gmail.com>
2014-07-15 14:52:00 +0800

    Separated issue card into another class

--- src/ui/IssuePanelCell.java
+++ src/ui/IssuePanelCell.java
@@ -1,33 +1,20 @@
 package ui;
 
-import java.lang.ref.WeakReference;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.beans.value.WeakChangeListener;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.Model;
 import model.TurboIssue;
-import model.TurboUser;
 
 public class IssuePanelCell extends ListCell<TurboIssue> {
 
 //	private final Stage mainStage;
 //	private final Model model;
 	private final int parentColumnIndex;
-	
-	private ChangeListener<String> titleChangeListener;
-	
+		
 	public IssuePanelCell(Stage mainStage, Model model, IssuePanel parent, int parentColumnIndex) {
 		super();
 //		this.mainStage = mainStage;
@@ -35,70 +22,16 @@ public class IssuePanelCell extends ListCell<TurboIssue> {
 		this.parentColumnIndex = parentColumnIndex;
 	}
 
-	private ChangeListener<String> createIssueTitleListener(TurboIssue issue, Text issueName){
-		WeakReference<TurboIssue> issueRef = new WeakReference<TurboIssue>(issue);
-		titleChangeListener = new ChangeListener<String>() {
-			@Override
-			public void changed(
-					ObservableValue<? extends String> stringProperty,
-					String oldValue, String newValue) {
-				TurboIssue issue = issueRef.get();
-				if(issue != null){
-					issueName.setText("#" + issue.getId() + " " + newValue);
-				}
-			}
-		};
-		
-		return titleChangeListener;
-	}
 	@Override
 	public void updateItem(TurboIssue issue, boolean empty) {
 		super.updateItem(issue, empty);
 		if (issue == null)
 			return;
 		
-		Text issueTitle = new Text("#" + issue.getId() + " " + issue.getTitle());
-		issueTitle.setWrappingWidth(330);
-		issueTitle.getStyleClass().add("issue-panel-name");
-		if (!issue.getOpen()) issueTitle.getStyleClass().add("issue-panel-closed");
-		issue.titleProperty().addListener(new WeakChangeListener<String>(createIssueTitleListener(issue, issueTitle)));
-
-		LabelDisplayBox labels = new LabelDisplayBox(issue.getLabelsReference(), false, "");
-
-		IssueIndicatorsDisplayBox indicators = new IssueIndicatorsDisplayBox(issue, false);
-		
-		TurboUser assignee = issue.getAssignee();
-		HBox rightAlignBox = new HBox();
-		rightAlignBox.setAlignment(Pos.BASELINE_RIGHT);
-		HBox.setHgrow(rightAlignBox, Priority.ALWAYS);
-		if (assignee != null) {
-			Label assigneeName = new Label(assignee.getGithubName());
-			assigneeName.getStyleClass().add("display-box-padding");
-			rightAlignBox.getChildren().addAll(assigneeName);
-		}
-
-		HBox leftAlignBox = new HBox();
-		leftAlignBox.setAlignment(Pos.BASELINE_LEFT);
-		HBox.setHgrow(leftAlignBox, Priority.ALWAYS);
-		leftAlignBox.getChildren().add(indicators);
-
-		HBox bottom = new HBox();
-		bottom.setSpacing(5);
-		bottom.getChildren().add(leftAlignBox);
-		if (assignee != null) bottom.getChildren().add(rightAlignBox);
-		
-		VBox issueCard = new VBox();
-		issueCard.setMaxWidth(350);
-		issueCard.getStyleClass().addAll("borders", "rounded-borders");
-		issueCard.setPadding(new Insets(5,5,5,5));
-		issueCard.setSpacing(5);
-		issueCard.getChildren().addAll(issueTitle, labels, bottom);
-
-		setGraphic(issueCard);
-		
+		setGraphic(new IssuePanelCard(issue));
 		setAlignment(Pos.CENTER);
 		
-		registerEvents(issue);
+//		registerEvents(issue);
 		
 		setOnDragDetected((event) -> {
 			Dragboard db = startDragAndDrop(TransferMode.MOVE);
@@ -116,7 +49,7 @@ public class IssuePanelCell extends ListCell<TurboIssue> {
 		});
 	}
 
-	private void registerEvents(TurboIssue issue) {
+//	private void registerEvents(TurboIssue issue) {
 //		WeakReference<TurboIssue> issueRef = new WeakReference<TurboIssue>(issue);
 //		setOnMouseClicked((MouseEvent mouseEvent) -> {
 //			if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
@@ -125,7 +58,7 @@ public class IssuePanelCell extends ListCell<TurboIssue> {
 //				}
 //			}
 //		});
-	}
+//	}
 
 //	private void onDoubleClick(TurboIssue issue) {
 //		TurboIssue oldIssue = new TurboIssue(issue);
