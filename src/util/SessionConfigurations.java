c0a44d9781fc91b638ddc02c9785d356d8b27f84
jwwong <jwwong.91@gmail.com>
2014-07-16 10:19:19 +0800

    Changed SessionCofigurations to a nonstatic class

--- src/util/SessionConfigurations.java
+++ src/util/SessionConfigurations.java
@@ -6,17 +6,17 @@ import java.util.List;
 import org.eclipse.egit.github.core.IRepositoryIdProvider;
 
 public class SessionConfigurations {
-	private static HashMap<String, List<String>> projectFilters;
+	private HashMap<String, List<String>> projectFilters;
 	
 	SessionConfigurations(HashMap<String, List<String>> projectFilters) {
-		SessionConfigurations.projectFilters = projectFilters;
+		this.projectFilters = projectFilters;
 	}
 	
-	public static void setFiltersForNextSession(IRepositoryIdProvider project, List<String> filter) {
+	public void setFiltersForNextSession(IRepositoryIdProvider project, List<String> filter) {
 		projectFilters.put(project.generateId(), filter);
 	}
 	
-	public static List<String> getFiltersFromPreviousSession(IRepositoryIdProvider project) {
+	public List<String> getFiltersFromPreviousSession(IRepositoryIdProvider project) {
 		return projectFilters.get(project.generateId());
 	}
 
