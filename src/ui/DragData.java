89f029f7c11edf320374af79f7d8f91f5186c4ea
Darius Foo <darius.foo.tw@gmail.com>
2014-07-21 13:32:40 +0800

    Updated DragData with relevant fields

--- src/ui/DragData.java
+++ src/ui/DragData.java
@@ -1,19 +1,30 @@
 package ui;
 
+import model.TurboLabel;
+import model.TurboMilestone;
+import model.TurboUser;
+
 import com.google.gson.Gson;
 
 public class DragData {
 	public enum Source {
-		ISSUE_CARD, COLUMN
+		ISSUE_CARD, COLUMN, LABEL_TAB, MILESTONE_TAB, ASSIGNEE_TAB
 	}
 	private Source source;
 	private int columnIndex;
 	private int issueIndex;
 	
+	private String entityName;
+	
 	public DragData(Source source, int col, int issue) {
 		this.setSource(source);
 		this.columnIndex = col;
 		this.issueIndex = issue;
+	}
+
+	public DragData(Source source, String name) {
+		this.setSource(source);
+		this.entityName = name;
 	}
 
 	public int getColumnIndex() {
@@ -46,5 +57,9 @@ public class DragData {
 
 	public void setSource(Source source) {
 		this.source = source;
+	}
+
+	public String getEntityName() {
+		return entityName;
 	}
 }
