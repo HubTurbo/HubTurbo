b7507daf6ad5117a19826fc534569a1704c5f916
Darius Foo <darius.foo.tw@gmail.com>
2014-12-11 15:04:43 +0800

    IOUtilities class

---
+++ src/util/IOUtilities.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class IOUtilities {
+	public static ByteArrayOutputStream inputStreamToByteArrayOutputStream(InputStream is) throws IOException {
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		int nRead;
+		byte[] data = new byte[4096];
+		while ((nRead = is.read(data, 0, data.length)) != -1) {
+		  buffer.write(data, 0, nRead);
+		}
+		buffer.flush();
+		return buffer;
+	}
+}
