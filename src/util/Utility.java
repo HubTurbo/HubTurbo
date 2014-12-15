502bd5c457ba5aedc17480ba2b79bf6a1a4e7546
Darius Foo <darius.foo.tw@gmail.com>
2014-12-15 13:46:25 +0800

    Added utility methods for getting screen dimensions

--- src/util/Utility.java
+++ src/util/Utility.java
@@ -1,7 +1,14 @@
 package util;
 
+import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.swing.UIManager;
 
 public class Utility {
 	
@@ -14,6 +21,22 @@ public class Utility {
         }
         m.appendTail(sb);
         return sb.toString();
+	}
+	
+	public static Rectangle getScreenDimensions() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		return new Rectangle((int) screenSize.getWidth(), (int) screenSize.getHeight());
+	}
+	
+	public static Optional<Rectangle> getUsableScreenDimensions() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			return Optional.of(ge.getMaximumWindowBounds());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Optional.empty();
 	}
 
 }
