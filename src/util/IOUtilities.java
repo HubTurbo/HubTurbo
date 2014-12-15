1d401fe856672fcec42fa6163bbedb0a448e527e
Darius Foo <darius.foo.tw@gmail.com>
2014-12-15 17:57:51 +0800

    Utility method for reading resources

--- src/util/IOUtilities.java
+++ src/util/IOUtilities.java
@@ -1,8 +1,14 @@
 package util;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
+
+import ui.UI;
 
 public class IOUtilities {
 	public static ByteArrayOutputStream inputStreamToByteArrayOutputStream(InputStream is) throws IOException {
@@ -14,5 +20,25 @@ public class IOUtilities {
 		}
 		buffer.flush();
 		return buffer;
+	}
+	
+	public static Optional<String> readResource(String packagePath) {
+		ClassLoader classLoader = UI.class.getClassLoader();
+		File file = new File(classLoader.getResource(packagePath).getFile());
+		BufferedReader reader;
+		StringBuilder sb = new StringBuilder();
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+				sb.append("\n");
+			}
+			reader.close();
+			return Optional.of(sb.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Optional.empty();
 	}
 }
