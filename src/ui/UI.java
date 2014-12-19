1c57aa8b4446ee82f034d939178eefbd03362812
Darius Foo <darius.foo.tw@gmail.com>
2014-12-19 17:52:11 +0800

    V0.7.15

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -36,7 +36,7 @@ import com.google.common.eventbus.EventBus;
 
 public class UI extends Application {
 
-	private static final String VERSION_NUMBER = " V0.7.14";
+	private static final String VERSION_NUMBER = " V0.7.15";
 	private static final double WINDOW_DEFAULT_PROPORTION = 0.6;
 
 	private static final Logger logger = LogManager.getLogger(UI.class.getName());
