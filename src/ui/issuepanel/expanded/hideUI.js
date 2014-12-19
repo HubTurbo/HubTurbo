b10708d44c24d8076725a9b82ebb0019b3d17eb6
Darius Foo <darius.foo.tw@gmail.com>
2014-12-19 16:11:19 +0800

    Re-exposed parts of the GitHub issue UI

--- src/ui/issuepanel/expanded/hideUI.js
+++ src/ui/issuepanel/expanded/hideUI.js
@@ -6,15 +6,11 @@ var paths = [
     // watch/star/fork buttons
     "body > div.wrapper > div.site > div.pagehead.repohead.instapaper_ignore.readability-menu > div > ul",
 
-    // edit and new issue buttons
-    "#partial-discussion-header > div.gh-header-show > div > button",
-    "#partial-discussion-header > div.gh-header-show > div > a",
-
-    // sidebar
-    "#discussion_bucket > div.discussion-sidebar",
-
-    // right navbar
     "body > div.wrapper > div.site > div.container > div.repository-with-sidebar.repo-container.new-discussion-timeline > div.repository-sidebar.clearfix > nav",
+    //
+    //#discussion_bucket > div.discussion-sidebar
+    // right navbar
+    //"body > div.wrapper > div.site > div.container > div.repository-with-sidebar.repo-container.new-discussion-timeline > div.repository-sidebar.clearfix > nav",
 
     // select parts of the footer
     "body > div.container > div > ul:nth-child(3) > li:nth-child(2) > a",
