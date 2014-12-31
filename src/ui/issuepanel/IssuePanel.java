19d0f82fff11c5f1895882855226f243e201e32c
Darius Foo <darius.foo.tw@gmail.com>
2014-12-31 16:09:59 +0800

    Fixed selection being lost or jumping to the top of the list when HubTurbo refreshes; fixes #277, #270

--- src/ui/issuepanel/IssuePanel.java
+++ src/ui/issuepanel/IssuePanel.java
@@ -1,6 +1,7 @@
 package ui.issuepanel;
 
 import java.lang.ref.WeakReference;
+import java.util.Optional;
 
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
@@ -15,6 +16,7 @@ import ui.issuecolumn.ColumnControl;
 import ui.issuecolumn.IssueColumn;
 import ui.sidepanel.SidePanel;
 import util.events.IssueSelectedEvent;
+import util.events.IssueSelectedEventHandler;
 
 import command.TurboCommandExecutor;
 
@@ -28,6 +30,7 @@ public class IssuePanel extends IssueColumn {
 	private final UI ui;
 
 	private ListView<TurboIssue> listView;
+	private Optional<Integer> selectedId = Optional.empty();
 	
 	public IssuePanel(UI ui, Stage mainStage, Model model, ColumnControl parentColumnControl, SidePanel sidePanel, int columnIndex, TurboCommandExecutor dragAndDropExecutor) {
 		super(ui, mainStage, model, parentColumnControl, sidePanel, columnIndex, dragAndDropExecutor);
@@ -42,12 +45,20 @@ public class IssuePanel extends IssueColumn {
 		setupListView();
 		getChildren().add(listView);
 		
+		ui.registerEvent(new IssueSelectedEventHandler() {
+			@Override
+			public void handle(IssueSelectedEvent e) {
+				selectedId = Optional.of(e.id);
+			}
+		});
+		
 		refreshItems();
 	}
 	
 	@Override
 	public void deselect() {
 		listView.getSelectionModel().clearSelection();
+		selectedId = Optional.empty();
 	}
 	
 	@Override
@@ -72,18 +83,76 @@ public class IssuePanel extends IssueColumn {
 		listView.setItems(null);
 		listView.setItems(getIssueList());
 	}
+	
+	/**
+	 * Given an issue id, returns the index of that issue's cell in this panel.
+	 */
+	private int getIndexOfIssue(int issueId) {
+		assert selectedId.isPresent() : "There has to be a previously selected id by this point";
+		int i = 0;
+		for (TurboIssue issue : getIssueList()) {
+			if (issue.getId() == selectedId.get()) {
+				return  i;
+			}
+			i++;
+		}
+		assert false : "This method should be called with an issue index that is in this panel! " + issueId;
+		return -1;
+	}
 
 	private void setupListView() {
 		setVgrow(listView, Priority.ALWAYS);
 		setOnKeyReleased((e) -> {
 			if (e.getCode().equals(KeyCode.DOWN) || e.getCode().equals(KeyCode.UP)) {
-				if (!e.isShiftDown()) {
-					TurboIssue selectedIssue = listView.getSelectionModel().selectedItemProperty().get();
-					if (selectedIssue != null) {
-						ui.triggerEvent(new IssueSelectedEvent(selectedIssue.getId()));
-					}
-				}
+				handleUpDownNavigation(e.getCode().equals(KeyCode.DOWN), e.isShiftDown());
 			}
 		}); 
+	}
+
+	/**
+	 * This method was written to deal with the problem of list view selection 'jumping'
+	 * to the top of the list when the list refreshes. The reason for this is that the data
+	 * structure backing the list is cleared when the refresh happens and the list cells recreated,
+	 * so the selection model is reset.
+	 * 
+	 * The problem is fixed by maintaining our own 'selection model': the selectedId field.
+	 * It tracks where the selection should be at any point in time. This allows us to set it if
+	 * it ever jumps to the top.
+	 * 
+	 * The rest of it is dealing with edge cases and interpreting what the list view is trying to
+	 * tell us.
+	 */
+	private void handleUpDownNavigation(boolean down, boolean shiftPressed) {
+		if (selectedId.isPresent()) {
+			
+			// Compute the next index based on the direction key, then clamp it to the size of the list
+			int correctIndex = getIndexOfIssue(selectedId.get()) + (down ? 1 : -1);
+			correctIndex = Math.max(0, Math.min(getIssueList().size()-1, correctIndex));
+			
+			// Model index is inconsistent with correct one => selection has jumped to the top.
+			// Select the correct item.
+			int modelIndex = listView.getSelectionModel().getSelectedIndex();
+			if (modelIndex != correctIndex) {
+				listView.getSelectionModel().clearAndSelect(correctIndex);
+			}
+			
+			// If jump happened, the selected item may be null
+			if (listView.getSelectionModel().getSelectedItem() != null) {
+				// If it's not, the selection model can be trusted, so we use that to update
+				// the selected id
+				selectedId = Optional.of(listView.getSelectionModel().getSelectedItem().getId());
+			} else {
+				// If it's null, we keep the previous selected id and do nothing
+			}
+			
+			// Trigger selection event for the right issue
+			if (!shiftPressed) {
+				ui.triggerEvent(new IssueSelectedEvent(selectedId.get(), columnIndex));
+			}
+
+		} else {
+			// Nothing is selected; do nothing
+			assert listView.getSelectionModel().getSelectedItem() == null : "There can't be a selected item if there is a previous selectedId";
+		}
 	}
 }
