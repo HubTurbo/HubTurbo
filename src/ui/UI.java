4cb735bbb009d91522b6ff1629bdbd8c941535a0
Darius Foo <darius.foo.tw@gmail.com>
2015-01-07 17:48:52 +0800

    V1.1.0

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -42,7 +42,7 @@ import com.google.common.eventbus.EventBus;
 public class UI extends Application {
 
 	private static final int VERSION_MAJOR = 1;
-	private static final int VERSION_MINOR = 0;
+	private static final int VERSION_MINOR = 1;
 	private static final int VERSION_PATCH = 0;
 	
 	public static final String ARG_UPDATED_TO = "--updated-to";
