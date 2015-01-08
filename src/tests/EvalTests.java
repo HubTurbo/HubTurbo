9fa951ad07b9c325f4622641df93c8aea9164f62
Darius Foo <darius.foo.tw@gmail.com>
2015-01-08 16:04:44 +0800

    Updated tests

--- src/tests/EvalTests.java
+++ src/tests/EvalTests.java
@@ -1,4 +1,4 @@
-package filter;
+package tests;
 
 import static org.junit.Assert.assertEquals;
 import model.TurboIssue;
@@ -6,15 +6,90 @@ import model.TurboIssue;
 import org.junit.Test;
 
 import stubs.ModelStub;
+import filter.Parser;
 import filter.expression.Qualifier;
 
 
 public class EvalTests {
 
+
+    private final ModelStub model = new ModelStub();
+
     @Test
-    public void basics() {
-    	ModelStub model = new ModelStub();
+    public void id() {
     	TurboIssue issue = new TurboIssue("1", "desc", model);
-    	assertEquals(Qualifier.process(Parser.parse("id:1"), issue), false);
+    	issue.setId(1);
+
+    	assertEquals(Qualifier.process(Parser.parse("id:1"), issue), true);
+    	assertEquals(Qualifier.process(Parser.parse("id:a"), issue), false);
     }
+
+    @Test
+    public void keyword() {
+    }
+    
+    @Test
+    public void title() {
+    }
+
+    @Test
+    public void body() {
+    }
+
+    @Test
+    public void milestone() {
+    }
+
+    @Test
+    public void parent() {
+    }
+
+    @Test
+    public void label() {
+    }
+
+    @Test
+    public void assignee() {
+    }
+    
+    @Test
+    public void author() {
+    }
+
+    @Test
+    public void involves() {
+    }
+
+    @Test
+    public void state() {
+    }
+
+    @Test
+    public void has() {
+    }
+
+    @Test
+    public void no() {
+    }
+
+    @Test
+    public void in() {
+    }
+    
+    @Test
+    public void type() {
+    }
+
+    @Test
+    public void is() {
+    }
+
+    @Test
+    public void created() {
+    }
+
+    @Test
+    public void updated() {
+    }
+
 }
