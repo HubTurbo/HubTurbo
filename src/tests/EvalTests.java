95099db6b6be25bafa1d8e6fafc3c4c808df6a0c
Darius Foo <darius.foo.tw@gmail.com>
2015-01-08 16:30:35 +0800

    Added tests up to milestones

--- src/tests/EvalTests.java
+++ src/tests/EvalTests.java
@@ -1,11 +1,13 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import model.TurboIssue;
+import model.TurboMilestone;
 
 import org.junit.Test;
 
 import stubs.ModelStub;
+import filter.ParseException;
 import filter.Parser;
 import filter.expression.Qualifier;
 
@@ -21,23 +23,63 @@ public class EvalTests {
     	issue.setId(1);
 
     	assertEquals(Qualifier.process(Parser.parse("id:1"), issue), true);
+    	
+    	// Non-number
     	assertEquals(Qualifier.process(Parser.parse("id:a"), issue), false);
     }
 
+    /**
+     * Tests for the presence of keywords in a particular issue.
+     * @param issue
+     */
+    private void testForKeywords(TurboIssue issue) {
+    	assertEquals(Qualifier.process(Parser.parse("test"), issue), true);
+
+    	// Substring
+    	assertEquals(Qualifier.process(Parser.parse("te"), issue), true);
+    	
+    	// Implicit conjunction
+    	assertEquals(Qualifier.process(Parser.parse("is a"), issue), true);
+    	
+    	// Like above but out of order
+    	assertEquals(Qualifier.process(Parser.parse("a is"), issue), true);    	
+    }
+    
     @Test
     public void keyword() {
+    	TurboIssue issue = new TurboIssue("", "this is a test", model);
+    	testForKeywords(issue);
     }
     
     @Test
     public void title() {
+    	TurboIssue issue = new TurboIssue("this is a test", "", model);
+    	testForKeywords(issue);
     }
 
     @Test
     public void body() {
+    	TurboIssue issue = new TurboIssue("", "this is a test", model);
+    	testForKeywords(issue);
     }
 
     @Test
     public void milestone() {
+    	TurboMilestone milestone = new TurboMilestone("v1.0");
+    	model.addMilestone(milestone);
+
+    	TurboIssue issue = new TurboIssue("", "", model);
+    	issue.setMilestone(milestone);
+
+    	assertEquals(Qualifier.process(Parser.parse("milestone:v1.0"), issue), true);
+    	assertEquals(Qualifier.process(Parser.parse("milestone:v1"), issue), true);
+    	assertEquals(Qualifier.process(Parser.parse("milestone:1"), issue), false);
+    	try {
+        	assertEquals(Qualifier.process(Parser.parse("milestone:."), issue), true);
+        	fail(". is not a valid token on its own");
+    	} catch (ParseException e) {
+    	}
+    	assertEquals(Qualifier.process(Parser.parse("milestone:what"), issue), false);
     }
 
     @Test
