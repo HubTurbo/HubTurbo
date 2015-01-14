0ac52aae7af64781a766e92e1943ee7194ec8fa3
Darius Foo <darius.foo.tw@gmail.com>
2015-01-14 21:04:00 +0800

    Added utility functions to ColumnControl for closing and opening sets of panels

--- src/ui/issuecolumn/ColumnControl.java
+++ src/ui/issuecolumn/ColumnControl.java
@@ -125,6 +125,19 @@ public class ColumnControl extends HBox {
 		return (Column) getChildren().get(index);
 	}
 	
+	public void closeAllColumns() {
+		getChildren().clear();
+		// There aren't any children left, so we don't need to update indices
+		ui.triggerEvent(new ColumnChangeEvent());
+	}
+	
+	public void openColumnsWithFilters(List<String> filters) {
+		for (String filter : filters) {
+			IssueColumn column = addColumn();
+			column.filterByString(filter);
+		}
+	}
+
 	public void closeColumn(int index) {
 		getChildren().remove(index);
 		updateColumnIndices();
