b25fee6927b39e8c3acaae0f3f0305023f7569d0
Darius Foo <darius.foo.tw@gmail.com>
2015-02-22 22:41:41 +0800

    Added a description to PlatformEx

--- src/util/PlatformEx.java
+++ src/util/PlatformEx.java
@@ -9,6 +9,12 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * Extensions to JavaFX's Platform class.
+ *
+ * Contains utility methods for running code in various ways,
+ * on or off (but close to) the JavaFX application thread.
+ */
 public class PlatformEx {
 
 	private static final Logger logger = LogManager.getLogger(PlatformEx.class.getName());
