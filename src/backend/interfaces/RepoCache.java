c3285e095fd1efe4edcfc74b6716a3e58b2a7eed
Darius Foo <darius.foo.tw@gmail.com>
2015-05-13 17:14:23 +0800

    Simplified RepoCache work queue

--- src/backend/interfaces/RepoCache.java
+++ src/backend/interfaces/RepoCache.java
@@ -10,33 +10,14 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public abstract class RepoCache {
 
-	private final LinkedBlockingQueue<CacheTask> tasks = new LinkedBlockingQueue<>();
-	private final ExecutorService pool = Executors.newFixedThreadPool(1);
-
-	public RepoCache() {
-		init();
-	}
-
-	public void init() {
-		pool.execute(this::handleTask);
-	}
-
-	public void handleTask() {
-		try {
-			tasks.take().run();
-			// Recurse
-			pool.execute(this::handleTask);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+	private final ExecutorService pool = Executors.newSingleThreadExecutor();
 
 	public static String escapeRepoName(String repoName) {
 		return repoName.replace("/", "-") + ".json";
 	}
 
 	protected void addTask(CacheTask task) {
-		tasks.add(task);
+		pool.execute(task);
 	}
 
 	public abstract boolean isRepoCached(String repoId);
