77e60bd8c0b3caadb5c492a7d2da11d3891670af
Darius Foo <darius.foo.tw@gmail.com>
2015-05-26 15:02:36 +0800

    Fixed edge cases with label name parsing

--- src/main/java/backend/resource/TurboLabel.java
+++ src/main/java/backend/resource/TurboLabel.java
@@ -90,18 +90,50 @@ public class TurboLabel {
 
 	public Optional<String> getGroup() {
 		if (getDelimiter().isPresent()) {
+			String delimiter = getDelimiter().get();
 			// Escaping due to constants not being valid regexes
-			return Optional.of(actualName.split("\\" + getDelimiter().get())[0]);
+			String[] segments = actualName.split("\\" + delimiter);
+			assert segments.length >= 1;
+			if (segments.length == 1) {
+				if (actualName.endsWith(delimiter)) {
+					// group.
+					return Optional.of(segments[0]);
+				} else {
+					// .name
+					return Optional.empty();
+				}
+			} else {
+				// group.name
+				assert segments.length == 2;
+				return Optional.of(segments[0]);
+			}
 		} else {
+			// name
 			return Optional.empty();
 		}
 	}
 
 	public String getName() {
 		if (getDelimiter().isPresent()) {
+			String delimiter = getDelimiter().get();
 			// Escaping due to constants not being valid regexes
-			return actualName.split("\\" + getDelimiter().get())[1];
+			String[] segments = actualName.split("\\" + delimiter);
+			assert segments.length >= 1;
+			if (segments.length == 1) {
+				if (actualName.endsWith(delimiter)) {
+					// group.
+					return "";
+				} else {
+					// .name
+					return segments[0];
+				}
+			} else {
+				// group.name
+				assert segments.length == 2;
+				return segments[1];
+			}
 		} else {
+			// name
 			return actualName;
 		}
 	}
