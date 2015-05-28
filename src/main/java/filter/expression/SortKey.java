8ba43b69039dfe789f6d7b102974214bb9192cfc
Darius Foo <darius.foo.tw@gmail.com>
2015-05-28 12:59:45 +0800

    Added SortKey toString, equals, hashCode

--- src/main/java/filter/expression/SortKey.java
+++ src/main/java/filter/expression/SortKey.java
@@ -8,4 +8,29 @@ public class SortKey {
 		this.key = key;
 		this.inverted = inverted;
 	}
+
+	@Override
+	public String toString() {
+		return (inverted ? "~" : "") + key;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		SortKey sortKey = (SortKey) o;
+
+		if (inverted != sortKey.inverted) return false;
+		if (key != null ? !key.equals(sortKey.key) : sortKey.key != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = key != null ? key.hashCode() : 0;
+		result = 31 * result + (inverted ? 1 : 0);
+		return result;
+	}
 }
