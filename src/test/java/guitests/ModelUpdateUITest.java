fafd4e18577b450364997d6b7818b2793c4bf960
Joshua Lee <me@jolly.sg>
2015-05-29 15:20:12 +0800

    updated ModelUpdateUITest to test for addition of issue and reset of repo

--- src/test/java/guitests/ModelUpdateUITest.java
+++ src/test/java/guitests/ModelUpdateUITest.java
@@ -1,24 +1,35 @@
 package guitests;
 
+import junit.framework.Assert;
 import org.junit.Test;
-import ui.RepositorySelector;
 import ui.UI;
+import ui.issuepanel.IssuePanel;
 import util.events.UILogicRefreshEvent;
 import util.events.UpdateDummyRepoEvent;
 
 public class ModelUpdateUITest extends UITest {
+
     @Test
-    public void modelUpdateUITest() throws InterruptedException {
-        RepositorySelector repositorySelector = find("#repositorySelector");
-        click(repositorySelector);
-        for (int i = 0; i < 10; i++) {
-            click("View");
-            click("Refresh");
-            sleep(500);
-            UI.events.triggerEvent(new UpdateDummyRepoEvent(UpdateDummyRepoEvent.UpdateType.NEW_ISSUE, "dummy/dummy"));
-            UI.events.triggerEvent(new UILogicRefreshEvent());
-            sleep(500);
-        }
-        sleep(1000);
+    public void addIssueTest() throws InterruptedException {
+        UI.events.triggerEvent(new UpdateDummyRepoEvent(UpdateDummyRepoEvent.UpdateType.RESET_REPO, "dummy/dummy"));
+        UI.events.triggerEvent(new UILogicRefreshEvent());
+        sleep(500);
+        UI.events.triggerEvent(new UpdateDummyRepoEvent(UpdateDummyRepoEvent.UpdateType.NEW_ISSUE, "dummy/dummy"));
+        UI.events.triggerEvent(new UILogicRefreshEvent());
+        sleep(500);
+        IssuePanel issuePanel = find("#dummy/dummy_col0");
+        Assert.assertEquals(issuePanel.getIssueCount(), 11);
+    }
+
+    @Test
+    public void countIssuesTest() throws InterruptedException {
+        UI.events.triggerEvent(new UpdateDummyRepoEvent(UpdateDummyRepoEvent.UpdateType.NEW_ISSUE, "dummy/dummy"));
+        UI.events.triggerEvent(new UILogicRefreshEvent());
+        sleep(500);
+        UI.events.triggerEvent(new UpdateDummyRepoEvent(UpdateDummyRepoEvent.UpdateType.RESET_REPO, "dummy/dummy"));
+        UI.events.triggerEvent(new UILogicRefreshEvent());
+        sleep(500);
+        IssuePanel issuePanel = find("#dummy/dummy_col0");
+        Assert.assertEquals(issuePanel.getIssueCount(), 10);
     }
 }
