4be90c19394a9b3003afc0425daa39248f59c8d6
Joshua Lee <me@jolly.sg>
2015-06-02 14:13:06 +0800

    added createLabelTest and createMilestoneTest

--- src/test/java/guitests/EventTests.java
+++ src/test/java/guitests/EventTests.java
@@ -3,25 +3,53 @@ package guitests;
 import org.junit.Test;
 import ui.UI;
 import util.events.IssueCreatedEventHandler;
+import util.events.LabelCreatedEventHandler;
+import util.events.MilestoneCreatedEventHandler;
 
 import static org.junit.Assert.assertEquals;
 
 public class EventTests extends UITest {
 
-    public static int createIssueCount;
+    public static int eventTestCount;
 
-    public static void increaseCreateIssueCount() {
-        createIssueCount++;
+    public static void increaseEventTestCount() {
+        eventTestCount++;
+    }
+
+    private static void resetEventTestCount() {
+        eventTestCount = 0;
     }
 
     @Test
     public void createIssueTest() {
-        createIssueCount = 0;
+        resetEventTestCount();
         UI.events.registerEvent((IssueCreatedEventHandler) e -> {
-            EventTests.increaseCreateIssueCount();
+            EventTests.increaseEventTestCount();
         });
         click("New");
         click("Issue");
-        assertEquals(1, createIssueCount);
+        assertEquals(1, eventTestCount);
+    }
+
+    @Test
+    public void createLabelTest() {
+        resetEventTestCount();
+        UI.events.registerEvent((LabelCreatedEventHandler) e -> {
+            EventTests.increaseEventTestCount();
+        });
+        click("New");
+        click("Label");
+        assertEquals(1, eventTestCount);
+    }
+
+    @Test
+    public void createMilestoneTest() {
+        resetEventTestCount();
+        UI.events.registerEvent((MilestoneCreatedEventHandler) e -> {
+            EventTests.increaseEventTestCount();
+        });
+        click("New");
+        click("Milestone");
+        assertEquals(1, eventTestCount);
     }
 }
