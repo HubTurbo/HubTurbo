51d517316082f0a0287f78b84290ef42ab1c61a5
Joshua Lee <me@jolly.sg>
2015-06-03 12:27:09 +0800

    implemented findElementById in ChromeDriverStub

--- src/main/java/browserview/ChromeDriverStub.java
+++ src/main/java/browserview/ChromeDriverStub.java
@@ -15,14 +15,99 @@ public class ChromeDriverStub extends ChromeDriver{
         super();
     }
 
-    @Override
-    public void quit() {}
+//    @Override
+//    public void quit() {}
 
     @Override
     public void get(String url) {}
 
     @Override
+    public String getCurrentUrl() {
+        return "https://github.com/HubTurbo/HubTurbo/issues/1";
+    }
+
+    @Override
     public WebElement findElement(By by) {
+        return new WebElement() {
+            @Override
+            public void click() {
+
+            }
+
+            @Override
+            public void submit() {
+
+            }
+
+            @Override
+            public void sendKeys(CharSequence... keysToSend) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public String getTagName() {
+                return null;
+            }
+
+            @Override
+            public String getAttribute(String name) {
+                return null;
+            }
+
+            @Override
+            public boolean isSelected() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+
+            @Override
+            public String getText() {
+                return null;
+            }
+
+            @Override
+            public List<WebElement> findElements(By by) {
+                return null;
+            }
+
+            @Override
+            public WebElement findElement(By by) {
+                return null;
+            }
+
+            @Override
+            public boolean isDisplayed() {
+                return false;
+            }
+
+            @Override
+            public Point getLocation() {
+                return null;
+            }
+
+            @Override
+            public Dimension getSize() {
+                return null;
+            }
+
+            @Override
+            public String getCssValue(String propertyName) {
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public WebElement findElementById(String using) {
         return new WebElement() {
             @Override
             public void click() {
