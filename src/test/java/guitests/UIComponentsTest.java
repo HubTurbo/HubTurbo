5b8f5debe9f846a7364f5499419d7955abd24066
Joshua Lee <me@jolly.sg>
2015-06-03 18:03:51 +0800

    moved out menuControlTest to its own test class

--- src/test/java/guitests/UIComponentsTest.java
+++ src/test/java/guitests/UIComponentsTest.java
@@ -31,32 +31,4 @@ public class UIComponentsTest extends UITest {
         press(KeyCode.SPACE).release(KeyCode.SPACE).press(KeyCode.SPACE).release(KeyCode.SPACE);
     }
 
-    @Test
-    public void menuControlTest() {
-        click("Panels");
-        click("Create");
-        click("Panels");
-        click("Create (Left)");
-        click("Panels");
-        click("Close");
-        click("Panels");
-        click("Close");
-
-        click("Boards");
-        click("Save");
-        type("Board 1");
-        click("OK");
-        click("Boards");
-        click("Open");
-        click("Board 1");
-        click("Boards");
-        click("Delete");
-        click("Board 1");
-        click("OK");
-
-        click("View");
-        click("Refresh");
-        click("View");
-        click("Force Refresh");
-    }
 }
