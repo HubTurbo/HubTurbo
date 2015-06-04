177645c94a199ced889836edb4b211f751c0234d
acruis <datsquall@gmail.com>
2015-06-04 17:41:59 +0800

    Added comments to clarify purpose of MultiModel

--- src/main/java/backend/interfaces/IModel.java
+++ src/main/java/backend/interfaces/IModel.java
@@ -6,6 +6,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+/**
+ * Only implemented by MultiModel.
+ * This interface's purpose is to hide the public methods of MultiModel from
+ * the UI.
+  */
 public interface IModel extends IBaseModel {
 	public String getDefaultRepo();
 	public void setDefaultRepo(String repoId);
