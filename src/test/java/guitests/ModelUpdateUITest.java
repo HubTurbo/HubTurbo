c6ce0403762a78009dbec23a0e5b00e5e0930f6f
Joshua Lee <me@jolly.sg>
2015-06-04 13:07:42 +0800

    reduced EVENT_DELAY in ModelUpdateUITest to 100ms

--- src/test/java/guitests/ModelUpdateUITest.java
+++ src/test/java/guitests/ModelUpdateUITest.java
@@ -14,7 +14,7 @@ import java.util.concurrent.FutureTask;
 
 public class ModelUpdateUITest extends UITest {
 
-    private final int EVENT_DELAY = 1000;
+    private final int EVENT_DELAY = 100;
 
     @Test
     @SuppressWarnings("unchecked")
