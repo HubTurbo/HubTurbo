0a9907f5c05a7b9683a59a80d2f6092c53fbdccb
Joshua Lee <me@jolly.sg>
2015-06-08 14:21:49 +0800

    removed redundant modifiers in FilterExpression

--- src/main/java/filter/expression/FilterExpression.java
+++ src/main/java/filter/expression/FilterExpression.java
@@ -13,36 +13,36 @@ public interface FilterExpression {
     // Determines if an issue satisfies this filter expression.
     // If so, it is shown in the issue panel.
 
-    public boolean isSatisfiedBy(IModel model, TurboIssue issue, MetaQualifierInfo info);
+    boolean isSatisfiedBy(IModel model, TurboIssue issue, MetaQualifierInfo info);
 
     // Filter expressions may only be applied if they contain no ambiguity
     // => they must contain only qualifiers or conjunctions thereof. Disjunctions
     // and negations can't be interpreted in order to be applied.
 
-    public boolean canBeAppliedToIssue();
+    boolean canBeAppliedToIssue();
 
     // Applies the traits that this filter expression expresses to
     // an issue. This should be invoked for disjunctions and negations
     // (i.e. call the above method to check first).
 
-    public void applyTo(TurboIssue issue, IModel model) throws QualifierApplicationException;
+    void applyTo(TurboIssue issue, IModel model) throws QualifierApplicationException;
 
     // Walks the syntax tree to get all the qualifier names that appear.
 
-    public List<String> getQualifierNames();
+    List<String> getQualifierNames();
 
     // A given FilterExpression has a string form that, when parsed, is
     // guaranteed to produce an equivalent FilterExpression. It can thus
     // be used to serialise FilterExpressions.
 
-    public String toString();
+    String toString();
 
     // Filters the syntax tree with a given predicate, returning a new FilterExpression.
     // New FilterExpressions are copied shallowly.
 
-    public FilterExpression filter(Predicate<Qualifier> pred);
+    FilterExpression filter(Predicate<Qualifier> pred);
 
     // Gets references to all qualifiers in a syntax tree matching a given predicate.
 
-    public List<Qualifier> find(Predicate<Qualifier> pred);
+    List<Qualifier> find(Predicate<Qualifier> pred);
 }
