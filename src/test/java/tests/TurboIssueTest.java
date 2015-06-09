5f1d6da264e7456ac081f8738ea7a30ef54eaf8b
Joshua Lee <me@jolly.sg>
2015-06-09 15:49:02 +0800

    extended TurboIssueTest

--- src/test/java/tests/TurboIssueTest.java
+++ src/test/java/tests/TurboIssueTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import backend.resource.TurboIssue;
 import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.Label;
 import org.eclipse.egit.github.core.User;
 import org.junit.Test;
 
@@ -20,9 +21,14 @@ public class TurboIssueTest {
         issue.setCreatedAt(new Date());
         issue.setUpdatedAt(new Date());
         issue.setState("open");
-        issue.setLabels(new ArrayList<>());
+        ArrayList<Label> labels = new ArrayList<>();
+        labels.add(new Label().setName("test label"));
+        issue.setLabels(labels);
         TurboIssue turboIssue = new TurboIssue("dummy/dummy", issue);
         assertEquals(1, turboIssue.getId());
+        assertEquals("test_user", turboIssue.getCreator());
+        assertEquals(true, turboIssue.isOpen());
+        assertEquals("test label", turboIssue.getLabels().get(0));
     }
 
 }
