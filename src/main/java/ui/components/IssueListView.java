80590f495f19e20e19b081f4f973661ca2239a5d
Joshua Lee <me@jolly.sg>
2015-06-10 18:14:37 +0800

    changed IssueListView equality

--- src/main/java/ui/components/IssueListView.java
+++ src/main/java/ui/components/IssueListView.java
@@ -6,8 +6,7 @@ public class IssueListView extends NavigableListView<TurboIssue> {
 
     boolean areItemsEqual(TurboIssue item1, TurboIssue item2) {
         return item1.getId() == item2.getId() &&
-                item1.getCreator().equals(item2.getCreator()) &&
-                item1.isPullRequest() == item2.isPullRequest();
+                item1.getRepoId().equals(item2.getRepoId());
     }
 
 }
