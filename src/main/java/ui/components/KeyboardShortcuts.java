682b3ebd01254557ece0143d3cbda82aae9f986e
Joshua Lee <me@jolly.sg>
2015-06-10 12:12:37 +0800

    added KeyboardShortcuts class

---
+++ src/main/java/ui/components/KeyboardShortcuts.java
@@ -0,0 +1,16 @@
+package ui.components;
+
+/**
+ * a central place to specify and detect keyboard shortcuts
+ *
+ * Classes that currently have keyboard shortcut code:
+ * ui.components.NavigableListView
+ * ui.issuecolumn.ColumnControl
+ * ui.issuepanel.IssuePanel
+ *
+ * Utility Class:
+ * util.KeyPress
+ */
+public class KeyboardShortcuts {
+
+}
