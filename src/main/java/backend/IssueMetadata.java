e367917a8f3ded88fce5036af61f3e376566c55e
acruis <datsquall@gmail.com>
2015-06-12 13:09:45 +0800

    Added field in IssueMetadata for last non-self update time

--- src/main/java/backend/IssueMetadata.java
+++ src/main/java/backend/IssueMetadata.java
@@ -1,5 +1,7 @@
 package backend;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,23 +12,38 @@ import github.TurboIssueEvent;
 public class IssueMetadata {
     private final List<TurboIssueEvent> events;
     private final List<Comment> comments;
+    private final LocalDateTime nonSelfUpdatedAt;
 
     // Constructor for default use when initializing TurboIssue
     public IssueMetadata() {
         events = new ArrayList<>();
         comments = new ArrayList<>();
+        nonSelfUpdatedAt = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.ofHours(0));
     }
 
     // Constructor used in DownloadMetadataTask
     public IssueMetadata(List<TurboIssueEvent> events, List<Comment> comments) {
         this.events = events;
         this.comments = comments;
+        this.nonSelfUpdatedAt = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.ofHours(0)); // Not calculated yet
+    }
+
+    // Constructor used in Logic
+    public IssueMetadata(IssueMetadata other, LocalDateTime lastNonSelfUpdatedAt) {
+        this.events = new ArrayList<>(other.events);
+        this.comments = new ArrayList<>(other.comments);
+        this.nonSelfUpdatedAt = lastNonSelfUpdatedAt; // Calculated just prior to calling this constructor
     }
 
     // Constructor used in MultiModel
     public IssueMetadata(IssueMetadata other) {
         this.events = new ArrayList<>(other.events);
         this.comments = new ArrayList<>(other.comments);
+        this.nonSelfUpdatedAt = other.nonSelfUpdatedAt;
+    }
+
+    public boolean isEmpty() {
+        return events.isEmpty() && comments.isEmpty();
     }
 
     public String summarise() {
@@ -39,6 +56,10 @@ public class IssueMetadata {
 
     public List<Comment> getComments() {
         return new ArrayList<>(comments);
+    }
+
+    public LocalDateTime getNonSelfUpdatedAt() {
+        return nonSelfUpdatedAt;
     }
 
     @Override
