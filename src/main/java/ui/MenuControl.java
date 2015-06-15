dcf87cb30d4ec400256756b16aeeb7bdc14078b1
hyungjon <hyungjon.kim@gmail.com>
2015-06-15 19:59:33 +0800

    Attempt to display login after logging out

--- src/main/java/ui/MenuControl.java
+++ src/main/java/ui/MenuControl.java
@@ -74,7 +74,7 @@ public class MenuControl extends MenuBar {
             logger.info("Logging out of HT");
             prefs.setLastLoginCredentials("", "");
             ui.quit();
-            // Need to do: clear the login credentials and return to login window
+            ui.main(null);
         });
         return logout;
 
