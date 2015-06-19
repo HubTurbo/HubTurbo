bf3cc169b86af82ef188f84e69288876389d650f
Joshua Lee <me@jolly.sg>
2015-06-19 14:49:11 +0800

    added showYesNoWarningDialog to DialogMessage

--- src/main/java/util/DialogMessage.java
+++ src/main/java/util/DialogMessage.java
@@ -1,12 +1,33 @@
 package util;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class DialogMessage {
-    public static void showErrorDialog(String title, String message){
+
+    public static void showErrorDialog(String header, String message){
         Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(title);
+        alert.setHeaderText(header);
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    public static boolean showYesNoWarningDialog
+            (String title, String header, String message, String yesButtonLabel, String noButtonLabel) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+
+        ButtonType yesButton = new ButtonType(yesButtonLabel);
+        ButtonType noButton = new ButtonType(noButtonLabel);
+        alert.getButtonTypes().setAll(yesButton, noButton);
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        return result.get().equals(yesButton);
+    }
+    
 }
