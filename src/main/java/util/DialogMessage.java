42b21bcb42ab25df964157330ce8d4a869f49c35
Joshua Lee <me@jolly.sg>
2015-06-19 14:10:18 +0800

    removed unused imports from DialogMessage

--- src/main/java/util/DialogMessage.java
+++ src/main/java/util/DialogMessage.java
@@ -1,9 +1,6 @@
 package util;
 
 import javafx.scene.control.Alert;
-import org.controlsfx.dialog.Dialogs;
-
-import javafx.concurrent.Task;
 
 public class DialogMessage {
     public static void showErrorDialog(String title, String message){
