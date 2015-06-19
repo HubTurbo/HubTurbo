27f4c0c28b4f81c2bb1de7fd46d61e32d9b686cd
Joshua Lee <me@jolly.sg>
2015-06-19 14:06:40 +0800

    updated DialogMessage to use the native javafx alert instead of controlsfx

--- src/main/java/util/DialogMessage.java
+++ src/main/java/util/DialogMessage.java
@@ -1,15 +1,16 @@
 package util;
 
+import javafx.scene.control.Alert;
 import org.controlsfx.dialog.Dialogs;
 
 import javafx.concurrent.Task;
 
 public class DialogMessage {
-    public static void showWarningDialog(String title, String message){
-        Dialogs.create()
-               .title(title)
-               .message(message)
-               .showWarning();
+    public static void showErrorDialog(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     public static void showProgressDialog(Task<?> task, String progressTitle){
