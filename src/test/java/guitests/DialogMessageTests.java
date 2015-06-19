2e8a329f5ad121c87849a61f47a6caba494c80dd
Joshua Lee <me@jolly.sg>
2015-06-19 16:26:05 +0800

    updated DialogMessageTests with assertNodeExists

--- src/test/java/guitests/DialogMessageTests.java
+++ src/test/java/guitests/DialogMessageTests.java
@@ -8,16 +8,16 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 import static org.junit.Assert.assertEquals;
+import static org.loadui.testfx.Assertions.assertNodeExists;
+import static org.loadui.testfx.controls.Commons.hasText;
 
 public class DialogMessageTests extends UITest {
 
     @Test
     public void showErrorDialogTest() {
-        Platform.runLater(() -> {
-            DialogMessage.showErrorDialog("Error", "Test Error Dialog");
-        });
+        Platform.runLater(() -> DialogMessage.showErrorDialog("Error", "Test Error Dialog"));
         sleep(1000);
-        click("Test Error Dialog");
+        assertNodeExists(hasText("Test Error Dialog"));
         click("OK");
     }
 
@@ -27,14 +27,14 @@ public class DialogMessageTests extends UITest {
                 DialogMessage.showYesNoWarningDialog("Warning", "Warning Header", "Warning Message", "yEs", "nO"));
         Platform.runLater(yesTask);
         sleep(1000);
-        click("Warning Header");
+        assertNodeExists(hasText("Warning Header"));
         click("yEs");
         assertEquals(true, yesTask.get());
         FutureTask<Boolean> noTask = new FutureTask<>(() ->
                 DialogMessage.showYesNoWarningDialog("Warning", "Warning Header", "Warning Message", "yEs", "nO"));
         Platform.runLater(noTask);
         sleep(1000);
-        click("Warning Message");
+        assertNodeExists(hasText("Warning Message"));
         click("nO");
         assertEquals(false, noTask.get());
     }
