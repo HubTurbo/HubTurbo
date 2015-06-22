ba6f776790830c7d711d9796181797f99bf94815
Joshua Lee <me@jolly.sg>
2015-06-22 17:14:13 +0800

    updated ChromeDriverTest to test for jump to comment

--- src/test/java/guitests/ChromeDriverTest.java
+++ src/test/java/guitests/ChromeDriverTest.java
@@ -10,6 +10,7 @@ import util.events.IssueSelectedEvent;
 import util.events.LabelCreatedEvent;
 import util.events.MilestoneCreatedEvent;
 import util.events.testevents.ExecuteScriptEventHandler;
+import util.events.testevents.JumpToCommentEventHandler;
 import util.events.testevents.NavigateToPageEventHandler;
 import util.events.testevents.SendKeysToBrowserEventHandler;
 
@@ -21,6 +22,7 @@ public class ChromeDriverTest extends UITest {
     private String url;
     private String script;
     private String keyCode;
+    private boolean jumpToComment;
 
     @Override
     public void launchApp() {
@@ -31,9 +33,13 @@ public class ChromeDriverTest extends UITest {
     @Test
     public void chromeDriverStubTest() {
         clearUrl();
+        clearScript();
+        clearKeyCode();
+        jumpToComment = false;
         UI.events.registerEvent((NavigateToPageEventHandler) e -> url = e.url);
         UI.events.registerEvent((ExecuteScriptEventHandler) e -> script = e.script);
         UI.events.registerEvent((SendKeysToBrowserEventHandler) e -> keyCode = e.keyCode);
+        UI.events.registerEvent((JumpToCommentEventHandler) e -> jumpToComment = true);
 
         UI.events.triggerEvent(new IssueSelectedEvent("dummy/dummy", 1, 0, false));
         sleep(EVENT_DELAY);
@@ -146,9 +152,9 @@ public class ChromeDriverTest extends UITest {
 
         // jump to comments
         push(KeyCode.C);
-//        sleep(EVENT_DELAY);
-//        assertEquals("c", keyCode);
-//        clearKeyCode();
+        sleep(EVENT_DELAY);
+        assertEquals(true, jumpToComment);
+        jumpToComment = false;
 
         click("View");
         click("Documentation");
