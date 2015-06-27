3ef8bc1a37804c4114f5e1e8931e5ef15e7a80b6
Su Gi Chandran <sollercoaster@hotmail.com>
2015-06-27 18:28:32 +0800

    Corrected style errors

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -425,7 +425,7 @@ public class UI extends Application implements EventDispatcher {
         logger.info("Triggered switch default repo");
         String[] openRepos = logic.getOpenRepositories().toArray(new String[0]);
         String currentRepo = logic.getDefaultRepo();
-    	
+        
         // Cycle to the next open repository
         for (int i = 0; i < openRepos.length; i++) {
             if (openRepos[i].equals(currentRepo)) {
