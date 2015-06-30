68eb2a5747f78d8f617015fb03ff52b44c2a1683
acruis <datsquall@gmail.com>
2015-06-30 11:28:01 +0800

    Renamed IssuePanelCell to ListPanelCell

--- src/main/java/ui/listpanel/ListPanelCell.java
+++ src/main/java/ui/listpanel/ListPanelCell.java
@@ -1,4 +1,4 @@
-package ui.issuepanel;
+package ui.listpanel;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -9,15 +9,15 @@ import backend.resource.TurboIssue;
 import javafx.geometry.Pos;
 import javafx.scene.control.ListCell;
 
-public class IssuePanelCell extends ListCell<TurboIssue> {
+public class ListPanelCell extends ListCell<TurboIssue> {
 
     private final IModel model;
     private final int parentColumnIndex;
-    private final IssuePanel parent;
+    private final ListPanel parent;
     private final HashSet<Integer> issuesWithNewComments;
 
-    public IssuePanelCell(IModel model, IssuePanel parent,
-                          int parentColumnIndex, HashSet<Integer> issuesWithNewComments) {
+    public ListPanelCell(IModel model, ListPanel parent,
+                         int parentColumnIndex, HashSet<Integer> issuesWithNewComments) {
         super();
         this.model = model;
         this.parent = parent;
@@ -37,7 +37,7 @@ public class IssuePanelCell extends ListCell<TurboIssue> {
         Optional<Model> currentModel = model.getModelById(issue.getRepoId());
         assert currentModel.isPresent() : "Invalid repo id " + issue.getRepoId()
             + " for issue " + issue.getId();
-        setGraphic(new IssuePanelCard(currentModel.get(), issue, parent, issuesWithNewComments));
+        setGraphic(new ListPanelCard(currentModel.get(), issue, parent, issuesWithNewComments));
         this.setId(issue.getRepoId() + "_col" + parentColumnIndex + "_" + issue.getId());
     }
 }
