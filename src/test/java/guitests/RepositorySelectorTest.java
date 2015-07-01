edc10da1ac48db7fdbfa8d47fe3cb5d1e25b6dc2
Joshua Lee <me@jolly.sg>
2015-07-01 14:11:01 +0800

    updated RepositorySelectorTest to check for current primary repo

--- src/test/java/guitests/RepositorySelectorTest.java
+++ src/test/java/guitests/RepositorySelectorTest.java
@@ -2,21 +2,58 @@ package guitests;
 
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
 import org.junit.Test;
+import org.loadui.testfx.utils.FXTestUtils;
+import ui.UI;
+import util.events.testevents.PrimaryRepoChangedEventHandler;
 
 import static org.junit.Assert.assertEquals;
 
 public class RepositorySelectorTest extends UITest {
 
+    private static String primaryRepo;
+
+    protected static class RepositorySelectorTestUI extends UI {
+        public RepositorySelectorTestUI() {
+            super();
+        }
+
+        @Override
+        public void start(Stage primaryStage) {
+            super.start(primaryStage);
+            stageFuture.set(primaryStage);
+        }
+
+        @Override
+        protected void registerTestEvents() {
+            UI.events.registerEvent((PrimaryRepoChangedEventHandler) e -> primaryRepo = e.repoId);
+        }
+    }
+
+    @Override
+    public void launchApp() {
+        FXTestUtils.launchApp(RepositorySelectorTestUI.class, "--test=true", "--bypasslogin=true");
+    }
+
     @Test
     public void repositorySelectorTest() {
         ComboBox<String> comboBox = find("#repositorySelector");
         assertEquals(1, comboBox.getItems().size());
-        doubleClick(comboBox);
-        doubleClick();
+        assertEquals("dummy/dummy", primaryRepo);
+        click("#dummy/dummy_col0_filterTextField");
+        type("repo");
+        press(KeyCode.SHIFT).press(KeyCode.SEMICOLON).release(KeyCode.SEMICOLON).release(KeyCode.SHIFT);
         type("dummy2/dummy2");
         push(KeyCode.ENTER);
-        sleep(5000);
+        assertEquals(2, comboBox.getItems().size());
+        assertEquals("dummy/dummy", primaryRepo);
+        doubleClick(comboBox);
+        doubleClick();
+        type("dummy3/dummy3");
+        push(KeyCode.ENTER);
+        assertEquals(3, comboBox.getItems().size());
+        assertEquals("dummy3/dummy3", primaryRepo);
     }
 
 }
