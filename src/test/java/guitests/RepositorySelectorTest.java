b881dc8c37c3fccadc9c694e785522d4370ce5ec
Joshua Lee <me@jolly.sg>
2015-07-01 14:37:07 +0800

    updated RepositorySelectorTest to check for login dialog when last viewed repo's json file does not exist and there are no other valid repo json files

--- src/test/java/guitests/RepositorySelectorTest.java
+++ src/test/java/guitests/RepositorySelectorTest.java
@@ -6,6 +6,8 @@ import javafx.stage.Stage;
 import org.eclipse.egit.github.core.RepositoryId;
 import org.junit.Test;
 import org.loadui.testfx.utils.FXTestUtils;
+import prefs.ConfigFileHandler;
+import prefs.GlobalConfig;
 import prefs.Preferences;
 import ui.UI;
 import util.events.testevents.PrimaryRepoChangedEventHandler;
@@ -14,6 +16,7 @@ import java.io.File;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.loadui.testfx.Assertions.assertNodeExists;
 
 public class RepositorySelectorTest extends UITest {
 
@@ -38,14 +41,37 @@ public class RepositorySelectorTest extends UITest {
 
     @Override
     public void launchApp() {
-        FXTestUtils.launchApp(RepositorySelectorTestUI.class, "--testconfig=true", "--bypasslogin=true");
+        FXTestUtils.launchApp(RepositorySelectorTestUI.class, "--testconfig=true");
+    }
+
+    @Override
+    public void setupMethod() {
+        // setup test json with last viewed repo "dummy/dummy"
+        // obviously the json for that repo doesn't exist
+        ConfigFileHandler configFileHandler =
+                new ConfigFileHandler(Preferences.DIRECTORY, Preferences.TEST_CONFIG_FILE);
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setLastLoginCredentials("test", "test");
+        globalConfig.setLastViewedRepository("dummy/dummy");
+        configFileHandler.saveGlobalConfig(globalConfig);
     }
 
     @Test
     public void repositorySelectorTest() {
+        // check if test json is present
+        File testConfig = new File(Preferences.DIRECTORY, Preferences.TEST_CONFIG_FILE);
+        if (!(testConfig.exists() && testConfig.isFile())) fail();
+
+        // now we check if the login dialog pops up because the "dummy/dummy" json
+        // doesn't exist and there are no other valid repo json files
+        assertNodeExists("#repoOwnerField");
+        type("dummy").push(KeyCode.TAB).type("dummy").push(KeyCode.ENTER);
         ComboBox<String> comboBox = find("#repositorySelector");
         assertEquals(1, comboBox.getItems().size());
         assertEquals("dummy/dummy", primaryRepo);
+
+        // we check if the "dummy2/dummy2" is added to the repository selector
+        // but the primary repo isn't changed
         click("#dummy/dummy_col0_filterTextField");
         type("repo");
         press(KeyCode.SHIFT).press(KeyCode.SEMICOLON).release(KeyCode.SEMICOLON).release(KeyCode.SHIFT);
@@ -53,6 +79,9 @@ public class RepositorySelectorTest extends UITest {
         push(KeyCode.ENTER);
         assertEquals(2, comboBox.getItems().size());
         assertEquals("dummy/dummy", primaryRepo);
+
+        // we check if "dummy3/dummy3" is added to the repository selector
+        // and that the primary repo is also changed
         doubleClick(comboBox);
         doubleClick();
         type("dummy3/dummy3");
@@ -66,7 +95,6 @@ public class RepositorySelectorTest extends UITest {
 
         // testing that the correct repo was saved in the json
         // check if the test JSON is still there...
-        File testConfig = new File(Preferences.DIRECTORY, Preferences.TEST_CONFIG_FILE);
         if (!(testConfig.exists() && testConfig.isFile())) fail();
 
         // ...then check that the JSON file contents are correct.
