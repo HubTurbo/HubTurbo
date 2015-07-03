3b562733497d8100cab6c72c606dc0ab2695e6ba
Joshua Lee <me@jolly.sg>
2015-07-03 18:07:01 +0800

    label picker dialog now pops up when triggered

--- src/main/java/ui/components/pickers/LabelPicker.java
+++ src/main/java/ui/components/pickers/LabelPicker.java
@@ -3,23 +3,35 @@ package ui.components.pickers;
 import backend.resource.TurboIssue;
 import backend.resource.TurboLabel;
 import javafx.application.Platform;
+import javafx.scene.control.Dialog;
+import javafx.stage.Modality;
+import javafx.stage.StageStyle;
+import javafx.util.Pair;
 import ui.UI;
 import util.events.ShowLabelPickerEventHandler;
 
-import java.util.List;
+import java.util.*;
 
 public class LabelPicker {
 
     private UI ui;
+    private Set<Pair<String, Integer>> openDialogs;
 
     public LabelPicker(UI ui) {
         this.ui = ui;
+        openDialogs = new HashSet<>();
         ui.registerEvent((ShowLabelPickerEventHandler) e -> Platform.runLater(() -> showLabelPicker(e.issue)));
     }
 
     private void showLabelPicker(TurboIssue issue) {
-        List<TurboLabel> allLabels = ui.logic.getRepo(issue.getRepoId()).getLabels();
-        ui.logic.replaceIssueLabels(issue, showLabelPickerDialog(issue, allLabels));
+        if (!openDialogs.contains(new Pair<>(issue.getRepoId(), issue.getId()))) {
+            openDialogs.add(new Pair<>(issue.getRepoId(), issue.getId()));
+            List<TurboLabel> allLabels = ui.logic.getRepo(issue.getRepoId()).getLabels();
+            ui.logic.replaceIssueLabels(issue, showLabelPickerDialog(issue, allLabels));
+            openDialogs.remove(new Pair<>(issue.getRepoId(), issue.getId()));
+        } else {
+            // focus on existing dialog
+        }
     }
 
     private List<String> showLabelPickerDialog(TurboIssue issue, List<TurboLabel> allLabels) {
@@ -29,6 +41,12 @@ public class LabelPicker {
         System.out.print("Current Labels: ");
         issue.getLabels().forEach(label -> System.out.print(label + " "));
         System.out.println();
+
+        Dialog labelPickerDialog = new Dialog();
+        labelPickerDialog.initStyle(StageStyle.UNIFIED);
+        labelPickerDialog.initModality(Modality.NONE);
+        labelPickerDialog.showAndWait();
+
         return issue.getLabels();
     }
 
