19b05fff68a3535c8aab299e6b1a4f52dd3d5c2f
Joshua Lee <me@jolly.sg>
2015-07-06 13:08:01 +0800

    first implementation of listview for labels in LabelPickerDialog

--- src/main/java/ui/components/pickers/LabelPickerDialog.java
+++ src/main/java/ui/components/pickers/LabelPickerDialog.java
@@ -3,17 +3,22 @@ package ui.components.pickers;
 import backend.resource.TurboIssue;
 import backend.resource.TurboLabel;
 import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LabelPickerDialog extends Dialog<List<String>> {
 
@@ -37,14 +42,29 @@ public class LabelPickerDialog extends Dialog<List<String>> {
         ButtonType confirmButtonType = new ButtonType("Confirm", ButtonBar.ButtonData.OK_DONE);
         getDialogPane().getButtonTypes().addAll(confirmButtonType, ButtonType.CANCEL);
 
-        VBox vBox = new VBox(10);
-        vBox.setAlignment(Pos.CENTER);
-        vBox.setPadding(new Insets(10, 200, 10, 0));
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(10));
         textField = new TextField();
-        textField.setMaxWidth(180);
-        textField.setMinWidth(180);
-        textField.setPrefWidth(180);
-        vBox.getChildren().add(textField);
+        textField.setPrefColumnCount(30);
+
+        ObservableList<Label> labels = FXCollections.observableArrayList
+                        (allLabels.stream()
+                        .map(label -> new Label(label.getActualName()))
+                        .collect(Collectors.toList()));
+        ListView<Label> labelList = new ListView<>(labels);
+        labelList.setCellFactory(CheckBoxListCell.forListView(Label::selectedProperty, new StringConverter<Label>() {
+            @Override
+            public String toString(Label object) {
+                return object.getName();
+            }
+
+            @Override
+            public Label fromString(String string) {
+                return null;
+            }
+        }));
+
+        vBox.getChildren().addAll(textField, labelList);
         getDialogPane().setContent(vBox);
 
         setResultConverter(dialogButton -> {
@@ -60,4 +80,36 @@ public class LabelPickerDialog extends Dialog<List<String>> {
     protected void requestFocus() {
         Platform.runLater(textField::requestFocus);
     }
+
+    public static class Label {
+
+        private ReadOnlyStringWrapper name = new ReadOnlyStringWrapper();
+        private BooleanProperty selected = new SimpleBooleanProperty(false);
+
+        public Label(String name) {
+            this.name.set(name);
+        }
+
+        public String getName() {
+            return name.get();
+        }
+
+        public ReadOnlyStringProperty nameProperty() {
+            return name.getReadOnlyProperty();
+        }
+
+        public BooleanProperty selectedProperty() {
+            return selected;
+        }
+
+        public boolean isSelected() {
+            return selected.get();
+        }
+
+        public void setSelected(boolean selected) {
+            this.selected.set(selected);
+        }
+
+    }
+
 }
