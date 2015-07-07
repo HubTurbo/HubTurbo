859d4de9a12d5bc6f3ba8dbf871688cd9cc7384a
Joshua Lee <me@jolly.sg>
2015-07-07 15:26:39 +0800

    removed ui.logic::refresh from replacedIssueLabels in LabelPicker

--- src/main/java/ui/components/pickers/LabelPicker.java
+++ src/main/java/ui/components/pickers/LabelPicker.java
@@ -38,7 +38,7 @@ public class LabelPicker {
             openDialogs.put(new Pair<>(issue.getRepoId(), issue.getId()), labelPickerDialog);
             Optional<List<String>> result = labelPickerDialog.showAndWait();
             if (result.isPresent()) {
-                ui.logic.replaceIssueLabels(issue, result.get()).thenRun(ui.logic::refresh);
+                ui.logic.replaceIssueLabels(issue, result.get());
             }
             openDialogs.remove(new Pair<>(issue.getRepoId(), issue.getId()));
         } else {
