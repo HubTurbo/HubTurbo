91308b7fd7570ba5c7ca71a8f8d68e8f7d52bee4
acruis <datsquall@gmail.com>
2015-07-07 14:45:15 +0800

    Added thenRun in showLabelPicker to avoid refresh race condition

--- src/main/java/ui/components/pickers/LabelPicker.java
+++ src/main/java/ui/components/pickers/LabelPicker.java
@@ -38,8 +38,7 @@ public class LabelPicker {
             openDialogs.put(new Pair<>(issue.getRepoId(), issue.getId()), labelPickerDialog);
             Optional<List<String>> result = labelPickerDialog.showAndWait();
             if (result.isPresent()) {
-                ui.logic.replaceIssueLabels(issue, result.get());
-                Platform.runLater(ui.logic::refresh);
+                ui.logic.replaceIssueLabels(issue, result.get()).thenRun(ui.logic::refresh);
             }
             openDialogs.remove(new Pair<>(issue.getRepoId(), issue.getId()));
         } else {
