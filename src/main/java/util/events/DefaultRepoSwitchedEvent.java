0e5589401086563b6f02876010401d902e7a4b6e
Su Gi Chandran <sollercoaster@hotmail.com>
2015-07-12 00:34:32 +0800

    Deleted DefaultRepoSwitchedEvent

--- src/main/java/util/events/DefaultRepoSwitchedEvent.java
+++
@@ -1,8 +0,0 @@
-package util.events;
-
-public class DefaultRepoSwitchedEvent extends Event {
-    public final String newDefaultRepoId;
-    public DefaultRepoSwitchedEvent(String repoId) {
-        this.newDefaultRepoId = repoId;
-    }
-}
--- src/main/java/util/events/DefaultRepoSwitchedEventHandler.java
+++
@@ -1,9 +0,0 @@
-package util.events;
-
-import com.google.common.eventbus.Subscribe;
-
-@FunctionalInterface
-public interface DefaultRepoSwitchedEventHandler extends EventHandler {
-    @Subscribe
-    void handle(DefaultRepoSwitchedEvent e);
-}
