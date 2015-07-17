130578be83c4525b2d7398ca7c7748ee10cc4b77
ndt93 <thiennguyen1993@gmail.com>
2015-07-17 18:29:27 +0800

    Add test for SerializableIssue's toString

--- src/test/java/tests/SerializationTests.java
+++ src/test/java/tests/SerializationTests.java
@@ -3,14 +3,22 @@ package tests;
 import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Optional;
 
+import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.Label;
 import org.eclipse.egit.github.core.Milestone;
+import org.eclipse.egit.github.core.User;
 import org.junit.Test;
 
+import backend.resource.TurboIssue;
 import backend.resource.TurboLabel;
 import backend.resource.TurboMilestone;
 import backend.resource.TurboUser;
+import backend.resource.serialization.SerializableIssue;
 import backend.resource.serialization.SerializableLabel;
 import backend.resource.serialization.SerializableMilestone;
 import backend.resource.serialization.SerializableUser;
@@ -70,7 +78,7 @@ public class SerializationTests {
         milestone.setState("open");
 
         TurboMilestone turboMilestone = new TurboMilestone("dummy/dummy", milestone);
-        turboMilestone.setDueDate(Optional.of(LocalDate.of(1, 1, 1)));
+        turboMilestone.setDueDate(Optional.of(LocalDate.of(1991, 1, 1)));
         turboMilestone.setDescription("test description");
         turboMilestone.setOpen(true);
         turboMilestone.setOpenIssues(0);
@@ -81,7 +89,7 @@ public class SerializationTests {
         String formatter = "Milestone: {%n"
                 + "  id: 1,%n"
                 + "  title: test milestone,%n"
-                + "  dueDate: 0001-01-01,%n"
+                + "  dueDate: 1991-01-01,%n"
                 + "  description: test description,%n"
                 + "  isOpen: true,%n"
                 + "  openIssues: 0,%n"
@@ -99,5 +107,54 @@ public class SerializationTests {
         SerializableUser serializedUser = new SerializableUser(user);
 
         assertEquals(expectedString, serializedUser.toString());
+    }
+
+    @Test
+    public void testSerializableIssueToString() {
+        Date createdDate = (new GregorianCalendar(1991, 5, 1, 2, 3, 4)).getTime();
+        Date updatedDate = (new GregorianCalendar(1991, 5, 2, 4, 3, 2)).getTime();
+
+        ArrayList<Label> labels = new ArrayList<>();
+        labels.add(new Label().setName("test label0"));
+        labels.add(new Label().setName("test label1"));
+
+        Issue issue = new Issue();
+        issue.setNumber(1);
+        issue.setUser(new User().setLogin("test_user"));
+        issue.setCreatedAt(createdDate);
+        issue.setUpdatedAt(updatedDate);
+        issue.setState("open");
+        issue.setLabels(labels);
+        issue.setTitle("test title");
+
+        TurboIssue turboIssue = new TurboIssue("dummy/dummy", issue);
+        turboIssue.setDescription("test description");
+        SerializableIssue serializedIssue = new SerializableIssue(turboIssue);
+
+        String formatter = "Issue: {%n"
+                 + "  id: 1,%n"
+                 + "  title: test title,%n"
+                 + "  creator: test_user,%n"
+                 + "  createdAt: 1991-06-01T02:03:04,%n"
+                 + "  isPullRequest: false,%n"
+                 + "  description: test description,%n"
+                 + "  updatedAt: 1991-06-02T04:03:02,%n"
+                 + "  commentCount: 0,%n"
+                 + "  isOpen: true,%n"
+                 + "  assignee: %s,%n"
+                 + "  labels: [test label0, test label1],%n"
+                 + "  milestone: %s,%n"
+                 + "}";
+
+        // Issue has no assignee and milestone
+        assertEquals(String.format(formatter, "", ""), serializedIssue.toString());
+
+        turboIssue.setAssignee("test assignee");
+        turboIssue.setMilestone(1);
+        serializedIssue = new SerializableIssue(turboIssue);
+
+        // Issue has assignee and milestone
+        assertEquals(String.format(formatter, "test assignee", "1"),
+                     serializedIssue.toString());
     }
 }
