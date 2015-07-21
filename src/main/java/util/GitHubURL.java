2d5e67cd3346b2d8436560b350d27fc428ff1b0c
acruis <datsquall@gmail.com>
2015-07-21 17:05:09 +0800

    Modified version logic in GitHubURL and tests

--- src/main/java/util/GitHubURL.java
+++ src/main/java/util/GitHubURL.java
@@ -2,15 +2,17 @@ package util;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ui.UI;
 
 public class GitHubURL {
     private static final Logger logger = LogManager.getLogger(GitHubURL.class.getName());
 
+    public static final String VERSION_NUMBER = UI.getCurrentVersion();
     public static final String LOGIN_PAGE = "https://github.com/login";
-    public static final String DOCS_PAGE = "https://github.com/HubTurbo/HubTurbo/blob/V3.0.0/docs/Getting-Started.md";
-    public static final String CHANGELOG_PAGE = "https://github.com/HubTurbo/HubTurbo/blob/V3.0.0/docs/Changelog.md";
+    public static final String DOCS_PAGE = "https://github.com/HubTurbo/HubTurbo/blob/%s/docs/Getting-Started.md";
+    public static final String CHANGELOG_PAGE = "https://github.com/HubTurbo/HubTurbo/blob/%s/docs/Changelog.md";
     public static final String CHANGELOG_PAGE_FORMAT =
-        "https://github.com/HubTurbo/HubTurbo/blob/V3.0.0/docs/Changelog.md#v%d%d%d";
+        "https://github.com/HubTurbo/HubTurbo/blob/%s/docs/Changelog.md#v%d%d%d";
 
     public static String getPathForAllIssues(String repoId) {
         return String.format("https://github.com/%s/issues", repoId);
@@ -37,7 +39,7 @@ public class GitHubURL {
     }
 
     public static String getPathForDocsPage() {
-        return DOCS_PAGE;
+        return String.format(DOCS_PAGE, VERSION_NUMBER);
     }
 
 //    public static String getChangelogForVersion(String version) {
@@ -46,10 +48,10 @@ public class GitHubURL {
 //            int major = numbers.get()[0];
 //            int minor = numbers.get()[1];
 //            int patch = numbers.get()[2];
-//            return String.format(CHANGELOG_PAGE_FORMAT, major, minor, patch);
+//            return String.format(CHANGELOG_PAGE_FORMAT, VERSION_NUMBER);
 //        } else {
 //            logger.error("Invalid version string format " + version + "; going to generic changelog page");
-//            return CHANGELOG_PAGE;
+//            return String.format(CHANGELOG_PAGE, VERSION_NUMBER);
 //        }
 //    }
 
@@ -58,7 +60,7 @@ public class GitHubURL {
     }
 
     public static String getPathForKeyboardShortcuts() {
-        return "https://github.com/HubTurbo/HubTurbo/wiki/Keyboard-Shortcuts";
+        return "https://github.com/HubTurbo/HubTurbo/blob/V3.0.0/docs/Keyboard-Shortcuts";
     }
 
     public static String getPathForMilestones(String repoId) {
