b765b9515823d444605c01caabe0b720591ddaf8
rd1992 <rajandhir92@gmail.com>
2015-08-14 22:06:51 +0800

    Changed shortcut for new comment

--- src/main/java/ui/components/KeyboardShortcuts.java
+++ src/main/java/ui/components/KeyboardShortcuts.java
@@ -82,7 +82,7 @@ public class KeyboardShortcuts {
     public static final KeyCode SHOW_CONTRIBUTORS = KeyCode.D;
 
     // TODO decouple manage/show labels/milestones?
-    public static final KeyCode NEW_COMMENT = KeyCode.C;
+    public static final KeyCode NEW_COMMENT = KeyCode.R;
     public static final KeyCode MANAGE_LABELS = KeyCode.L;
     public static final KeyCode MANAGE_ASSIGNEES = KeyCode.A;
     public static final KeyCode MANAGE_MILESTONE = KeyCode.M;
@@ -123,7 +123,7 @@ public class KeyboardShortcuts {
         assignedKeys.add(KeyCode.F5); //REFRESH
         assignedKeys.add(KeyCode.F1); //SHOW_DOCS
         assignedKeys.add(KeyCode.G); //GOTO_MODIFIER
-        assignedKeys.add(KeyCode.C); //NEW_COMMENT
+        assignedKeys.add(KeyCode.R); //NEW_COMMENT
         assignedKeys.add(KeyCode.A); //MANAGE_ASSIGNEES
         assignedKeys.add(KeyCode.SPACE); //DOUBLE_PRESS
     }
