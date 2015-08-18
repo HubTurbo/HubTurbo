826242e787b10d73cb43c24ef9bb3245623167ef
hyungjon <hyungjon.kim@gmail.com>
2015-08-18 16:54:23 +0800

    Renamed PanelInfoEmptyTest to NoPanelAtQuitTest

--- src/test/java/guitests/NoPanelAtQuitTest.java
+++ src/test/java/guitests/NoPanelAtQuitTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class PanelInfoEmptyTest extends UITest {
+public class NoPanelAtQuitTest extends UITest {
 
     @Override
     public void launchApp() {
