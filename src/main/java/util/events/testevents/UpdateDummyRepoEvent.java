e21c0838f75010c5b513edff170b69eed5c3d9ab
acruis <datsquall@gmail.com>
2015-08-20 04:12:03 +0800

    Add ADD_COMMENT as an UpdateDummyRepoEvent type
    Comments are currently only relevant to us in terms of their authors and
    creation dates, to display on the GUI and to process non-self update times
    of issues. Thus, the actor field is introduced to UpdateDummyRepoEvent to
    reflect the author of the commit, as well as to be used to specify the
    actor of a model update in general, if needed in the future.
    
    Since a new field is introduced, other constructors have to be modified to
    accommodate the field.

--- src/main/java/util/events/testevents/UpdateDummyRepoEvent.java
+++ src/main/java/util/events/testevents/UpdateDummyRepoEvent.java
@@ -9,11 +9,13 @@ public class UpdateDummyRepoEvent extends Event {
     public final int itemId; // For issues and milestones
     public final String idString; // For labels and users
     public final String updateText;
+    public final String actor;
 
     public enum UpdateType {NEW_ISSUE, UPDATE_ISSUE, DELETE_ISSUE,
         NEW_LABEL, DELETE_LABEL,
         NEW_MILESTONE, UPDATE_MILESTONE, DELETE_MILESTONE,
         NEW_USER, DELETE_USER,
+        ADD_COMMENT,
         RESET_REPO
     }
 
@@ -25,43 +27,50 @@ public class UpdateDummyRepoEvent extends Event {
      * @param itemId The id of the element to be updated or deleted, if necessary.
      * @param updateText The text to update the element with, if necessary.
      */
-    public UpdateDummyRepoEvent(UpdateType updateType, String repoId, int itemId, String idString, String updateText) {
+    public UpdateDummyRepoEvent(UpdateType updateType, String repoId, int itemId, String idString, String updateText,
+                                String actor) {
         this.updateType = updateType;
         this.repoId = repoId;
         this.itemId = itemId;
         this.idString = idString;
         this.updateText = updateText;
+        this.actor = actor;
     }
 
     // Overloaded constructors
 
     // Hardly used, maybe to clean up entire model
     public UpdateDummyRepoEvent(UpdateType updateType) {
-        this(updateType, null, -1, null, null);
+        this(updateType, null, -1, null, null, "");
     }
 
     // e.g. Clean repo, new issue
     public UpdateDummyRepoEvent(UpdateType updateType, String repoId) {
-        this(updateType, repoId, -1, null, null);
+        this(updateType, repoId, -1, null, null, "");
     }
 
-    // e.g. Delete issue
+    // e.g. Delete issue, delete milestone
     public UpdateDummyRepoEvent(UpdateType updateType, String repoId, int itemId) {
-        this(updateType, repoId, itemId, null, null);
+        this(updateType, repoId, itemId, null, null, "");
     }
 
     // e.g. Delete label
     public UpdateDummyRepoEvent(UpdateType updateType, String repoId, String idString) {
-        this(updateType, repoId, -1, idString, null);
+        this(updateType, repoId, -1, idString, null, "");
     }
 
-    // e.g. Update issue
+    // e.g. Update issue, update milestone
     public UpdateDummyRepoEvent(UpdateType updateType, String repoId, int itemId, String updateText) {
-        this(updateType, repoId, itemId, null, updateText);
+        this(updateType, repoId, itemId, null, updateText, "");
     }
 
     // e.g. Update label
     public UpdateDummyRepoEvent(UpdateType updateType, String repoId, String idString, String updateText) {
-        this(updateType, repoId, -1, idString, updateText);
+        this(updateType, repoId, -1, idString, updateText, "");
+    }
+
+    // e.g. Comment
+    public UpdateDummyRepoEvent(UpdateType updateType, String repoId, int itemId, String updateText, String actor) {
+        this(updateType, repoId, itemId, null, updateText, actor);
     }
 }
