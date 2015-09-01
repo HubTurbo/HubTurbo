560ebb17310ade7f809cb9eacd91a1dffe4187ec
acruis <datsquall@gmail.com>
2015-09-02 02:14:54 +0800

    Modify test mode API quota behaviour
    The test mode API quota now decreases whenever a getUpdatedEvent call is
    carried out for an issue with new updates since last metadata retrieval,
    and whenever getComments is called. When getUpdatedEvent is called for an
    issue that does not have new updates, the API quota will not decrease, as
    per the GitHub API behaviour.
    
    Other method calls such as getIssues or getMilestones also do not change
    the API quota.
    
    Additionally, the API box is assigned an ID so that tests can locate the
    API box and its text easily.

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -208,6 +208,7 @@ public class UI extends Application implements EventDispatcher {
     private void initUI(Stage stage) {
         repoSelector = createRepoSelector();
         apiBox = new Label("-/-");
+        apiBox.setId("apiBox");
 
         mainStage = stage;
         stage.setMaximized(false);
