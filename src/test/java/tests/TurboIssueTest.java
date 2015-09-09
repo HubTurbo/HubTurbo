f445851590d44f96ddf057ba81b439a83e1182f3
ndt93 <thiennguyen1993@gmail.com>
2015-09-09 23:53:22 +0800

    Add test for new isCurrentlyRead method

--- src/test/java/tests/TurboIssueTest.java
+++ src/test/java/tests/TurboIssueTest.java
@@ -6,12 +6,17 @@ import org.eclipse.egit.github.core.Label;
 import org.eclipse.egit.github.core.User;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TurboIssueTest {
+    private static String REPO = "testrepo/testrepo";
 
     @Test
     public void turboIssueTest() {
@@ -29,6 +34,33 @@ public class TurboIssueTest {
         assertEquals("test_user", turboIssue.getCreator());
         assertEquals(true, turboIssue.isOpen());
         assertEquals("test label", turboIssue.getLabels().get(0));
+    }
+
+    /**
+     * Tests TurboIssue's isCurrentRead method
+     */
+    @Test
+    public void testReadState() {
+        TurboIssue issue = new TurboIssue(REPO, 1, "", "",
+                LocalDateTime.of(2011, 1, 1, 1, 1, 1), false);
+
+        // An issue is not read if it doesn't record any markedReadAt time
+        assertFalse(issue.isCurrentlyRead());
+
+        // An issue is read if it has no updatedAt time
+        // and its markedAsRead time is after its createdAt time
+        issue.setMarkedReadAt(Optional.of(LocalDateTime.of(2015, 1, 6, 12, 15)));
+        issue.setUpdatedAt(null);
+        assertTrue(issue.isCurrentlyRead());
+
+        // An issue is not read if its markedAsRead time is before its updatedAt time
+        issue.setUpdatedAt(LocalDateTime.of(2015, 2, 17, 2, 10));
+        issue.setMarkedReadAt(Optional.of(LocalDateTime.of(2015, 1, 6, 12, 15)));
+        assertFalse(issue.isCurrentlyRead());
+
+        // An issue is marked as read if its markedAsRead time is after its updated Time
+        issue.setMarkedReadAt(Optional.of(LocalDateTime.of(2015, 3, 6, 12, 15)));
+        assertTrue(issue.isCurrentlyRead());
     }
 
 }
