7fd2eeb8f517523e4a5f2a77777e213eb185db0b
ndt93 <thiennguyen1993@gmail.com>
2015-09-15 19:32:29 +0800

    Create PullRequestServiceEx

--- src/main/java/github/PullRequestServiceEx.java
+++ src/main/java/github/PullRequestServiceEx.java
@@ -1,7 +1,66 @@
 package github;
 
-/**
- * Created by ndt on 9/14/15.
- */
-public class PullRequestServiceEx {
+import com.google.gson.reflect.TypeToken;
+import org.eclipse.egit.github.core.IRepositoryIdProvider;
+import org.eclipse.egit.github.core.client.PagedRequest;
+import org.eclipse.egit.github.core.service.PullRequestService;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.eclipse.egit.github.core.client.IGitHubConstants.*;
+
+public class PullRequestServiceEx extends PullRequestService {
+    /**
+     * Gets a pull request's review comments
+     *
+     * @param repository
+     * @param pullRequestNumber
+     * @return list of review comments
+     * @throws IOException
+     */
+    public List<ReviewComment> getReviewComments(IRepositoryIdProvider repository,
+                                                 int pullRequestNumber) throws IOException {
+        return getReviewComments(repository, Integer.toString(pullRequestNumber));
+    }
+
+    /**
+     * Gets a pull request's review comments
+     *
+     * @param repository
+     * @param pullRequestNumber
+     * @return list of review comments
+     * @throws IOException
+     */
+    public List<ReviewComment> getReviewComments(IRepositoryIdProvider repository,
+                                                 String pullRequestNumber) throws IOException {
+        String repoId = getId(repository);
+        return getReviewComments(repoId, pullRequestNumber);
+    }
+
+    /**
+     * Gets a pull request's review comments
+     *
+     * @param repoId
+     * @param pullRequestNumber
+     * @return list of review comments
+     * @throws IOException
+     */
+    private List<ReviewComment> getReviewComments(String repoId, String pullRequestNumber)
+            throws IOException {
+        StringBuilder uri = new StringBuilder(SEGMENT_REPOS);
+        uri.append('/').append(repoId);
+        uri.append(SEGMENT_PULLS);
+        uri.append('/').append(pullRequestNumber);
+        uri.append(SEGMENT_COMMENTS);
+
+        PagedRequest<ReviewComment> request = createPagedRequest();
+        request.setUri(uri);
+        request.setType(new TypeToken<List<ReviewComment>>() {
+        }.getType());
+
+        System.out.println(request.getUri());
+
+        return getAll(request);
+    }
 }
