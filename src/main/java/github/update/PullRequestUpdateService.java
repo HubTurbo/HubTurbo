14801a4706837d9b2c43097c5e976f802898feac
ndt93 <thiennguyen1993@gmail.com>
2015-09-26 21:26:43 +0800

    Add tests for getting review/commmit comments in GitHubRepo

--- src/main/java/github/update/PullRequestUpdateService.java
+++ src/main/java/github/update/PullRequestUpdateService.java
@@ -5,26 +5,20 @@ import github.GitHubClientEx;
 import org.eclipse.egit.github.core.IRepositoryIdProvider;
 import org.eclipse.egit.github.core.PullRequest;
 import org.eclipse.egit.github.core.client.PagedRequest;
-import util.Utility;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.eclipse.egit.github.core.client.IGitHubConstants.SEGMENT_PULLS;
 
 public class PullRequestUpdateService extends UpdateService<PullRequest> {
-    private final Date lastIssueCheckTime;
-
-    public PullRequestUpdateService(GitHubClientEx client, String pullRequestsETags, Date lastIssueCheckTime) {
+    public PullRequestUpdateService(GitHubClientEx client, String pullRequestsETags) {
         super(client, SEGMENT_PULLS, pullRequestsETags);
-        this.lastIssueCheckTime = new Date(lastIssueCheckTime.getTime());
     }
 
     private Map<String, String> createUpdatedPullRequestsParams(){
         Map<String, String> params = new HashMap<>();
-        params.put("since", Utility.formatDateISO8601(lastIssueCheckTime));
         params.put("state", "all");
         return params;
     }
