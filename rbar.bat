17bde683838bb5c7a6ce3ca0883eb324f628073c
acruis <datsquall@gmail.com>
2015-09-27 07:22:47 +0800

    Backup the user's directory before cleaning

--- rbar.bat
+++ rbar.bat
@@ -1,13 +1,22 @@
-@for /F "delims=" %%a in ('findstr /rc:"^    version" build.gradle') do @set versionline=%%a
-@echo Closing all java and javaw processes with window title format: HubTurbo V%versionline:~15,5%*.
+@for /r build\libs %%i in (*.jar) do @set versionline=%%~ni
+@echo Closing all java and javaw processes with window title format: HubTurbo V%versionline:~9,5%*.
 @taskkill /fi "Windowtitle eq HubTurbo V%versionline:~15,5%*" /im "javaw.exe"
 @taskkill /fi "Windowtitle eq HubTurbo V%versionline:~15,5%*" /im "java.exe"
+@echo(
+@echo Backing up your files. Any jar or exe files will not be backed up.
+@echo .jar>>rbar-ex.txt
+@echo .exe>>rbar-ex.txt
+@xcopy build\libs rbar /i /s /exclude:rbar-ex.txt
+@del rbar-ex.txt
 @echo(
 @echo Starting Gradle build. This will take a while...
 @echo(
 @call gradlew clean shadowJar --no-daemon
+@xcopy rbar build\libs /i /s
+@rmdir /S /Q rbar
 @for /F "delims=" %%a in ('findstr /rc:"^    version" build.gradle') do @set versionline=%%a
+@cd build\libs
 @echo(
 @echo Starting HubTurbo V%versionline:~15,5%. You can close this cmd window once HubTurbo begins launching.
-@start "" javaw -jar build/libs/HubTurbo-%versionline:~15,5%-all.jar
+@start "" javaw -jar HubTurbo-%versionline:~15,5%-all.jar
 @exit
