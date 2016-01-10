d36bc997e62c248021e591889901eacff976327c
acruis <datsquall@gmail.com>
2016-01-10 21:47:37 +0800

    Create GUIElement class
    Each GUIElement contains references to a TurboIssue, as well as related
    objects which are necessary to properly the TurboIssue as a panel card.
    These include:
    - Labels related to the issue (both current and removed)
    - Milestone of the issue (if exists)
    - Assignee of the issue (if exists)
    
    Removed labels of the issue are also necessary to display the issue
    metadata, in particular label removal events.
    
    GUIElements are meant to ultimately remove the references to the Model by
    instead passing only the references to the necessary objects within the
    Model. This means that together with model deletion, obsolete objects
    within a Model can be quickly (and safely) garbage collected.

---
+++ src/main/java/ui/GUIElement.java
@@ -0,0 +1,61 @@
+package ui;
+
+import backend.resource.TurboIssue;
+import backend.resource.TurboLabel;
+import backend.resource.TurboMilestone;
+import backend.resource.TurboUser;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class GUIElement {
+    private TurboIssue issue;
+    private List<TurboLabel> labels;
+    private Optional<TurboMilestone> milestone;
+    private Optional<TurboUser> assignee;
+
+    public GUIElement(TurboIssue issue,
+                      List<TurboLabel> labels,
+                      Optional<TurboMilestone> milestone,
+                      Optional<TurboUser> assignee) {
+        this.issue = issue;
+        this.labels = labels;
+        this.milestone = milestone;
+        this.assignee = assignee;
+    }
+
+    public TurboIssue getIssue() {
+        return issue;
+    }
+
+    /**
+     * Returns all labels related to an issue (including labels that have been added and then removed).
+     * Use getLabelsOfIssue to retrieve an issue's current labels instead.
+     *
+     * @return Labels related to an issue, or null if the GUIElement has been constructed as such.
+     */
+    public List<TurboLabel> getLabels() {
+        return labels;
+    }
+
+    public Optional<TurboMilestone> getMilestone() {
+        return milestone;
+    }
+
+    public Optional<TurboUser> getAssignee() {
+        return assignee;
+    }
+
+    public Optional<TurboLabel> getLabelByActualName(String actualName) {
+        return labels.stream().filter(label -> label.getActualName().equals(actualName)).findFirst();
+    }
+
+    public List<TurboLabel> getLabelsOfIssue(TurboIssue issue) {
+        return issue.getLabels().stream().map(labelName -> {
+            Optional<TurboLabel> label = getLabelByActualName(labelName); // O(n^2) for the additional assert.
+            assert label.isPresent();
+            return label.get();
+        }).collect(Collectors.toList());
+    }
+}
