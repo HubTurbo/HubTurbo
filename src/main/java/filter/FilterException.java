98b5e44c2b57b6bd7a3bc0c97c773062d74014f8
jinifief <jinified@gmail.com>
2016-01-22 19:26:10 +0800

    added FilterException

---
+++ src/main/java/filter/FilterException.java
@@ -0,0 +1,8 @@
+package filter;
+
+@SuppressWarnings("serial")
+public class FilterException extends RuntimeException {
+    public FilterException(String message) {
+        super(message);
+    }
+}
