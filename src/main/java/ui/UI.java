9eeaff13c0f2ff1eff81d4dc2d6d8d2ed348e837
Darius Foo <darius.foo.tw@gmail.com>
2016-01-26 22:14:45 +0800

    V3.17.0

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -54,7 +54,7 @@ import static ui.components.KeyboardShortcuts.SWITCH_DEFAULT_REPO;
 public class UI extends Application implements EventDispatcher {
 
     public static final int VERSION_MAJOR = 3;
-    public static final int VERSION_MINOR = 16;
+    public static final int VERSION_MINOR = 17;
     public static final int VERSION_PATCH = 0;
 
     public static final String ARG_UPDATED_TO = "--updated-to";
