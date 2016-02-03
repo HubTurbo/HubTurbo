724211681a7c8b8b3ec267f1f517dc08e1c916e7
garbanzos <chrno_crusade217@hotmail.com>
2016-02-04 01:19:40 +0800

    edited repoID class based on suggestions
    - removed repoIDString attribute
    - removed unnecessary imports from RepoID
    - constructors now throw IllegalArgumentExceptions
    - moved utility mtds to RepoID class

--- src/main/java/backend/RepoID.java
+++ src/main/java/backend/RepoID.java
@@ -1,33 +1,56 @@
 package backend;
 
-import util.Utility;
-
-import java.util.Optional;
+import org.eclipse.egit.github.core.RepositoryId;
 
 /**
  * Represents the ID of a repository. To be used to ensure safe comparison
  */
 public final class RepoID {
 
-    private final String repoIDString;
     private final String repoOwner;
     private final String repoName;
 
     /**
-     * Return an Optional with a new instance of repoID if repoIDString is valid
-     * @param repoIDString
-     * @return Optional<RepoID>
+     * Constructor
+     * @param repoIDString repoOwner and repoName separated by slash
+     * @throws IllegalArgumentException If repoISString is invalid
      */
-    public static Optional<RepoID> createRepoIDInstance(String repoIDString){
-        String lowerCaseRepoIDString = repoIDString.toLowerCase();
-        boolean isValidRepoIDString = Utility.isWellFormedRepoId(repoIDString);
-        if (isValidRepoIDString) {
-            String[] repoIDComponents = lowerCaseRepoIDString.split("/");
-            RepoID repoID = new RepoID(lowerCaseRepoIDString, repoIDComponents[0], repoIDComponents[1]);
-            return Optional.of(repoID);
+    public RepoID(String repoIDString) throws IllegalArgumentException {
+        boolean isValidRepoID = RepoID.isWellFormedRepoId(repoIDString);
+        if (isValidRepoID) {
+            String[] repoIDComponents = repoIDString.toLowerCase().split("/");
+            this.repoOwner = repoIDComponents[0];
+            this.repoName = repoIDComponents[1];
         } else {
-            return Optional.empty();
+            throw new IllegalArgumentException();
         }
+    }
+
+    /**
+     * Constructor
+     * @param repoOwner Owner of the repo
+     * @param repoName Name of the repo
+     * @throws IllegalArgumentException If repoOwner or repoName is invalid
+     */
+    public RepoID(String repoOwner, String repoName) throws IllegalArgumentException {
+        boolean isValidRepoID = RepoID.isWellFormedRepoId(repoOwner, repoName);
+        if (isValidRepoID) {
+            this.repoOwner = repoOwner.toLowerCase();
+            this.repoName = repoName.toLowerCase();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean isWellFormedRepoId(String owner, String repo) {
+        return !(owner == null || owner.isEmpty() || repo == null || repo.isEmpty())
+                && isWellFormedRepoId(RepositoryId.create(owner, repo).generateId());
+    }
+
+    public static boolean isWellFormedRepoId(String repoId) {
+        RepositoryId repositoryId = RepositoryId.createFromId(repoId);
+        return repoId != null && !repoId.isEmpty() && repositoryId != null
+                && repoId.equals(repositoryId.generateId());
     }
 
     public String getRepoOwner() {
@@ -39,7 +62,7 @@ public final class RepoID {
     }
 
     public String getRepoIDString() {
-        return repoIDString;
+        return repoOwner + "/" + repoName;
     }
 
     @Override
@@ -47,22 +70,14 @@ public final class RepoID {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RepoID repoID = (RepoID) o;
-        if (!repoIDString.equals(repoID.repoIDString)) return false;
         if (!repoOwner.equals(repoID.repoOwner)) return false;
         return repoName.equals(repoID.repoName);
     }
 
     @Override
     public int hashCode() {
-        int result = repoIDString.hashCode();
-        result = 31 * result + repoOwner.hashCode();
+        int result = repoOwner.hashCode();
         result = 31 * result + repoName.hashCode();
         return result;
-    }
-
-    private RepoID(String lowerCaseRepoIDString, String repoOwner, String repoName){
-        this.repoIDString = lowerCaseRepoIDString;
-        this.repoOwner = repoOwner;
-        this.repoName = repoName;
     }
 }
