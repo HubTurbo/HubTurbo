3017e2fb678d970b0ecbe90af9284fcd431e2b6e
jinifief <jinified@gmail.com>
2016-02-09 21:41:28 +0800

    Init test

---
+++ src/test/java/tests/IssueCreatorLogicTest.java
@@ -0,0 +1,38 @@
+package tests;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import backend.resource.Model;
+import backend.resource.TurboIssue;
+import ui.components.issue_creators.IssueCreatorPresenter;
+
+public class IssueCreatorLogicTest {
+
+    private Model repo;
+    private TurboIssue issue;
+    private IssueCreatorPresenter presenter;
+
+    @Before
+    public void init() {
+        repo = mock(Model.class);
+        issue = new TurboIssue("dummy/dummy", 1, "Old Issue");
+        presenter = new IssueCreatorPresenter(repo, Optional.of(issue));
+    }
+
+    @Test
+    public void isNewIssue() {
+        assertFalse(presenter.isNewIssue());
+    }
+    
+    @Test
+    public void resolveTitle() {
+        String expected = String.format("Editing Issue #%d", issue.getId());
+        assertEquals(expected, presenter.resolveIssueTitle());
+    }
+    
+}
