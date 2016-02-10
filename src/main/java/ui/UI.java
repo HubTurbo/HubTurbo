fdd1824c2dd0340fa1fb068c19c4cf4fa49c1b91
Hans Adrian <nick.hansadrian@gmail.com>
2016-02-10 17:04:49 +0800

    Created a file downloader module
    Added a file downloader module for update system

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -31,6 +31,8 @@ import ui.components.StatusUI;
 import ui.components.pickers.LabelPicker;
 import ui.issuepanel.PanelControl;
 import undo.UndoController;
+import updater.UpdateManager;
+import updater.UpdateManagerStub;
 import util.*;
 import util.events.*;
 import util.events.Event;
@@ -75,6 +77,7 @@ public class UI extends Application implements EventDispatcher {
     public GUIController guiController;
     private NotificationController notificationController;
     public UndoController undoController;
+    public UpdateManager updateManager;
 
 
     // Main UI elements
@@ -209,6 +212,9 @@ public class UI extends Application implements EventDispatcher {
 
         uiManager = new UIManager(this);
         status = new HTStatusBar(this);
+
+        updateManager = TestController.createUpdateManager();
+        updateManager.run();
     }
 
     private void initApplicationState() {
@@ -303,7 +309,7 @@ public class UI extends Application implements EventDispatcher {
         scene.getStylesheets().add(css);
     }
 
-    public static void loadFonts(){
+    public static void loadFonts() {
         Font.loadFont(UI.class.getResource("octicons/octicons-local.ttf").toExternalForm(), 32);
     }
 
@@ -539,5 +545,9 @@ public class UI extends Application implements EventDispatcher {
 
     public boolean isWindowFocused() {
         return mainStage.isFocused();
+    }
+    
+    public void showUpdateProgressWindow() {
+        updateManager.showUpdateProgressWindow();
     }
 }
