5b99ec11fdea9d8342e91e38d5f4073640bf5d13
jinifief <jinified@gmail.com>
2016-02-11 18:00:05 +0800

    Added context menu pop-up

--- src/main/java/ui/components/issue_creators/IssueContentPane.java
+++ src/main/java/ui/components/issue_creators/IssueContentPane.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.parboiled.matchervisitors.GetStarterCharVisitor;
 import org.pegdown.PegDownProcessor;
 
 import backend.resource.TurboIssue;
@@ -23,22 +24,28 @@ import javafx.scene.web.WebView;
  */
 public class IssueContentPane extends StackPane {
 
-    public static final String HTML_TEMPLATE = "<!DOCTYPE html><html><head></head><body>%s</body></html>";
+    public static final String HTML_TEMPLATE =
+            "<!DOCTYPE html><html><head></head><body>%s</body></html>";
     public static final String REFERENCE_ISSUE = "#%d %s";
     public static final int MAX_SUGGESTION_ENTRIES = 5;
+    // Magic number that accounts for width of each character in textarea
+    public static final int CARET_OFFSET = 8;
+    // Vertical offset for popup
+    public static final int VERTICAL_OFFSET = 20;
 
-    public static final KeyCodeCombination PREVIEW = 
-        new KeyCodeCombination(KeyCode.P, KeyCodeCombination.ALT_DOWN);
+    public static final KeyCodeCombination PREVIEW = new KeyCodeCombination(KeyCode.P,
+            KeyCodeCombination.ALT_DOWN);
     public static final KeyCodeCombination MENTION = new KeyCodeCombination(KeyCode.AT);
     public static final KeyCodeCombination REFERENCE = new KeyCodeCombination(KeyCode.POUND);
-    public static final KeyCodeCombination HIDE_SUGGGESTIONS = new KeyCodeCombination(KeyCode.SPACE);
-    
+    public static final KeyCodeCombination HIDE_SUGGGESTIONS =
+            new KeyCodeCombination(KeyCode.SPACE);
+
     private static final int COL_PREF_COUNT = 30;
 
-    private final PegDownProcessor markdownProcessor; 
+    private final PegDownProcessor markdownProcessor;
     private final TextArea body;
     private final WebView preview;
-    
+
     private final SuggestionMenu suggestions;
     private final IssueCreatorPresenter presenter;
 
@@ -55,7 +62,7 @@ public class IssueContentPane extends StackPane {
         generatePreview(content);
         setupHandlers();
     }
-    
+
     /**
      * Returns content in text area
      */
@@ -72,8 +79,13 @@ public class IssueContentPane extends StackPane {
     private void setupHandlers() {
         setOnMouseClicked(this::mouseClickHandler);
         setOnKeyPressed(this::keyPressHandler);
+        body.setOnKeyPressed(this::bodyKeyPressHandler);
     }
 
+
+    // ==============
+    // Event handlers
+    // ==============
 
     private void mouseClickHandler(MouseEvent e) {
         body.toFront();
@@ -87,14 +99,36 @@ public class IssueContentPane extends StackPane {
         e.consume();
     }
 
+    /**
+     * Triggers context menu for every keyword
+     */
+    private void bodyKeyPressHandler(KeyEvent e) {
+        Point2D pos = getPopupPosition(body.getCaretPosition());
+        suggestions.show(body, pos.getX(), pos.getY());
+    }
+
+    // ===========
+    // Suggestions
+    // ===========
+
+    /**
+     * Converts caret position from local offset to screen coordinate
+     * 
+     * @param caretPosition
+     * @return Point2D screen coordinates
+     */
+    private Point2D getPopupPosition(int caretPosition) {
+        return body.localToScreen(caretPosition * CARET_OFFSET,
+                Math.ceil(caretPosition / COL_PREF_COUNT) * VERTICAL_OFFSET);
+    }
+
     private void showSuggestions(List<String> searchResult) {
         suggestions.loadSuggestions(searchResult);
     }
 
     private List<String> getAllIssues(List<TurboIssue> issues) {
-        return issues.stream()
-            .map(i -> String.format(REFERENCE_ISSUE, i.getId(), i.getTitle()))
-            .collect(Collectors.toList());
+        return issues.stream().map(i -> String.format(REFERENCE_ISSUE, i.getId(), i.getTitle()))
+                .collect(Collectors.toList());
     }
 
     private List<String> getAllUsers(List<TurboUser> users) {
@@ -106,7 +140,7 @@ public class IssueContentPane extends StackPane {
     // =================
 
     /**
-     * Initializes WebView and set index 
+     * Initializes WebView and set index
      */
     private WebView initWebView() {
         WebView preview = new WebView();
@@ -115,17 +149,17 @@ public class IssueContentPane extends StackPane {
     }
 
     /**
-     * Initializes TextArea and set index 
+     * Initializes TextArea and set index
      */
     private TextArea initBody() {
         TextArea body = new TextArea();
         body.setPrefColumnCount(COL_PREF_COUNT);
         body.setWrapText(true);
-        suggestions.loadSuggestions(Arrays.asList("test", "babi","halo"));
+        suggestions.loadSuggestions(Arrays.asList("test", "babi", "halo"));
         this.getChildren().add(0, body);
         return body;
     }
-    
+
     private void togglePane() {
         preview.toFront();
     }
