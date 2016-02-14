7a4d5dba3f923d3002f2856ac0dffa54f03a15df
m133225 <m133225@hotmail.com>
2016-02-15 01:20:31 +0800

    Added milestone picker logic which relies on text input. Clicking of milestones is not working yet. Modified MilestonePickerDialog return type to Pair since we have to know both the button pressed and milestone selection result, which fixes removing milestone when Cancel is pressed.

--- src/main/java/ui/components/pickers/PickerMilestone.java
+++ src/main/java/ui/components/pickers/PickerMilestone.java
@@ -9,6 +9,8 @@ import javafx.scene.control.Label;
 public class PickerMilestone extends TurboMilestone implements Comparable<PickerMilestone> {
     boolean isSelected = false;
     boolean isHighlighted = false;
+    boolean isFaded = false;
+    boolean isExisting = false;
     MilestonePickerDialog dialog;
 
     PickerMilestone(TurboMilestone milestone, MilestonePickerDialog dialog) {
@@ -18,6 +20,20 @@ public class PickerMilestone extends TurboMilestone implements Comparable<Picker
         setOpen(milestone.isOpen());
         setOpenIssues(milestone.getOpenIssues());
         setClosedIssues(milestone.getClosedIssues());
+        this.dialog = dialog;
+    }
+
+    PickerMilestone(PickerMilestone milestone, MilestonePickerDialog dialog) {
+        super(milestone.getRepoId(), milestone.getId(), milestone.getTitle());
+        setDueDate(milestone.getDueDate());
+        setDescription(milestone.getDescription() == null ? "" : milestone.getDescription());
+        setOpen(milestone.isOpen());
+        setOpenIssues(milestone.getOpenIssues());
+        setClosedIssues(milestone.getClosedIssues());
+        setFaded(milestone.isFaded());
+        setHighlighted(milestone.isHighlighted());
+        setSelected(milestone.isSelected());
+        setExisting(milestone.isExisting());
         this.dialog = dialog;
     }
 
@@ -34,7 +50,56 @@ public class PickerMilestone extends TurboMilestone implements Comparable<Picker
         }
 
         if (isHighlighted) {
-            milestone.setStyle("-fx-border-color: black;");
+            milestone.setStyle(milestone.getStyle() + "-fx-border-color: black;");
+        }
+
+        if (isFaded) {
+            milestone.setStyle(milestone.getStyle() + "-fx-opacity: 40%;");
+        }
+
+        milestone.setOnMouseClicked(e -> dialog.toggleMilestone(getTitle()));
+
+        return milestone;
+    }
+
+    public Node getNewlyAssignedMilestoneNode(boolean hasSuggestion) {
+        Label milestone = new Label(getTitle());
+        FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
+        double width = fontLoader.computeStringWidth(milestone.getText(), milestone.getFont());
+        milestone.setPrefWidth(width + 30);
+        milestone.getStyleClass().add("labels");
+        milestone.setStyle("-fx-background-color: yellow;");
+
+        if (hasSuggestion) {
+            milestone.setStyle(milestone.getStyle() + "-fx-opacity: 40%;");
+            if (isSelected) {
+                milestone.getStyleClass().add("labels-removed"); // add strikethrough
+            }
+        }
+
+        if (isSelected) {
+            milestone.setStyle(milestone.getStyle() + "-fx-border-color: black;");
+        }
+
+        milestone.setOnMouseClicked(e -> dialog.toggleMilestone(getTitle()));
+
+        return milestone;
+    }
+
+    public Node getExistingMilestoneNode(boolean hasSuggestion) {
+        Label milestone = new Label(getTitle());
+        FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
+        double width = fontLoader.computeStringWidth(milestone.getText(), milestone.getFont());
+        milestone.setPrefWidth(width + 30);
+        milestone.getStyleClass().add("labels");
+        milestone.setStyle("-fx-background-color: yellow;");
+
+        if (isSelected && (hasSuggestion || isHighlighted)) {
+            milestone.setStyle(milestone.getStyle() + "-fx-opacity: 40%;");
+        }
+
+        if (!isExisting || !isSelected || hasSuggestion || isHighlighted) {
+            milestone.getStyleClass().add("labels-removed"); // add strikethrough
         }
 
         milestone.setOnMouseClicked(e -> dialog.toggleMilestone(getTitle()));
@@ -56,6 +121,21 @@ public class PickerMilestone extends TurboMilestone implements Comparable<Picker
 
     public boolean isHighlighted() {
         return this.isHighlighted;
+    }
+
+    public void setFaded(boolean isFaded) {
+        this.isFaded = isFaded;
+    }
+
+    public boolean isFaded() {
+        return this.isFaded;
+    }
+    public void setExisting(boolean isExisting) {
+        this.isExisting = isExisting;
+    }
+
+    public boolean isExisting() {
+        return this.isExisting;
     }
 
     /**
