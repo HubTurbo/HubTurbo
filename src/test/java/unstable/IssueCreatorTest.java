3063b6eff0fb02c7c5358d09e4bb308b8c5dd459
jinifief <jinified@gmail.com>
2016-02-15 11:07:42 +0800

    Added verifyEditIssueContent

--- src/test/java/unstable/IssueCreatorTest.java
+++ src/test/java/unstable/IssueCreatorTest.java
@@ -5,13 +5,13 @@ import static org.junit.Assert.*;
 import java.util.Optional;
 
 import javafx.application.Platform;
-import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import guitests.UITest;
 
 import org.junit.Test;
 
+import backend.resource.TurboIssue;
 import ui.UI;
 import ui.listpanel.ListPanelCell;
 import util.events.ShowIssueCreatorEvent;
@@ -34,12 +34,33 @@ public class IssueCreatorTest extends UITest {
         
         TextField title = find(TITLE_FIELD);
         verifyTextFieldInput(title, "hello");
-        
-        push(KeyCode.ESCAPE);
-        waitUntilNodeDisappears(TITLE_FIELD);
+        verifyCleanExit();
     }
 
     
+    @Test
+    public void verifyEditIssueContent() {
+        // Ensures the content and title matches issue being edited
+        ListPanelCell listPanelCell = find("#dummy/dummy_col0_9");
+        TurboIssue issue = listPanelCell.getIssue();
+        String assignee = "dummy";
+        int milestone = 0;
+        issue.setAssignee(assignee);
+        issue.setMilestone(milestone);
+
+        UI.events.triggerEvent(new ShowIssueCreatorEvent(Optional.of(issue)));
+        waitUntilNodeAppears(TITLE_FIELD);
+        
+        TextField title = find(TITLE_FIELD);
+        TextField assigneeField = find(ASSIGNEE_FIELD);
+        TextField milestoneField = find(MILESTONE_FIELD);
+
+        assertEquals(issue.getTitle(), title.getText());
+        assertEquals(assignee, assigneeField.getText());
+        assertEquals(milestone, Integer.parseInt(milestoneField.getText()));
+        verifyCleanExit();
+    }
+
     /**
      * Validates input to text field. Test fails when content does not match input.
      */
@@ -48,5 +69,10 @@ public class IssueCreatorTest extends UITest {
         type(content);
         
         assertEquals(content, field.getText());
+    }
+    
+    private void verifyCleanExit() {
+        push(KeyCode.ESCAPE);
+        waitUntilNodeDisappears(TITLE_FIELD);
     }
 }
