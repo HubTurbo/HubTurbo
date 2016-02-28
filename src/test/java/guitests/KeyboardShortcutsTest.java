4bc2f0a1228488043a3cf6bd1436cca1be6d9729
m133225 <m133225@hotmail.com>
2016-02-28 13:13:56 +0800

    Updated comments to have a simple description of the problem in getting issue 10's node.

--- src/test/java/guitests/KeyboardShortcutsTest.java
+++ src/test/java/guitests/KeyboardShortcutsTest.java
@@ -170,7 +170,8 @@ public class KeyboardShortcutsTest extends UITest {
         doubleClick(repoSelectorComboBox);
         pushKeys(SWITCH_DEFAULT_REPO);
         // wait for issue 9 to appear then click on it
-        // issue 9 is chosen instead of issue 10 due to a problem with finding issue 10's node
+        // issue 9 is chosen instead of issue 10
+        // as there is a problem with finding issue 10's node due to it being the first card in the panel
         waitUntilNodeAppears("#dummy/dummy_col1_9");
         assertEquals(repoSelectorComboBox.getValue(), "dummy/dummy");
         // test shortcut when focus is on panel
