03cc124cedd26b59f346e1e4e230a8262c65b1e5
m133225 <m133225@hotmail.com>
2016-02-29 15:47:45 +0800

    Updated KeyboardShortcutsTest line 140 and 143 to be relative to the number of dummy issues.

--- src/test/java/guitests/KeyboardShortcutsTest.java
+++ src/test/java/guitests/KeyboardShortcutsTest.java
@@ -137,10 +137,10 @@ public class KeyboardShortcutsTest extends UITest {
         // wait for issue 9 to appear then click on it
         // issue 9 is chosen instead of issue 10
         // as there is a problem with finding issue 10's node due to it being the first card in the panel
-        waitUntilNodeAppears("#dummy/dummy_col1_9");
+        waitUntilNodeAppears("#dummy/dummy_col1_" + (DummyRepoState.noOfDummyIssues - 1));
         assertEquals(repoSelectorComboBox.getValue(), "dummy/dummy");
         // test shortcut when focus is on panel
-        click("#dummy/dummy_col1_9");
+        click("#dummy/dummy_col1_" + (DummyRepoState.noOfDummyIssues - 1));
         press(SWITCH_DEFAULT_REPO);
         PlatformEx.waitOnFxThread();
         assertEquals(repoSelectorComboBox.getValue(), "dummy1/dummy1");
