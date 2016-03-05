08def69aad90f7ce2ee5f1972dccce59c7c4d26f
ndt <thiennguyen1993@gmail.com>
2016-03-05 23:47:40 +0800

    Remove UpdateModelOp

--- src/main/java/backend/control/operations/UpdateLocalModelOp.java
+++ src/main/java/backend/control/operations/UpdateLocalModelOp.java
@@ -1,19 +1,19 @@
 package backend.control.operations;
 
-import static util.Futures.chain;
-
-import java.util.concurrent.CompletableFuture;
-
 import backend.RepoIO;
 import backend.resource.Model;
 
-public class UpdateModelOp implements RepoOp<Model> {
+import java.util.concurrent.CompletableFuture;
+
+import static util.Futures.chain;
+
+public class UpdateLocalModelOp implements RepoOp<Model> {
 
     private final Model oldModel;
     private final RepoIO repoIO;
     private final CompletableFuture<Model> result;
 
-    public UpdateModelOp(Model oldModel, RepoIO repoIO, CompletableFuture<Model> result) {
+    public UpdateLocalModelOp(Model oldModel, RepoIO repoIO, CompletableFuture<Model> result) {
         this.oldModel = oldModel;
         this.repoIO = repoIO;
         this.result = result;
