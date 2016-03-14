f47e6f0f91b4a756655a6d3c3fdf45c5e6025670
Hans Adrian <nick.hansadrian@gmail.com>
2016-03-14 16:37:14 +0800

    Unnecessary import

--- src/test/java/tests/JavaVersionTest.java
+++ src/test/java/tests/JavaVersionTest.java
@@ -7,7 +7,6 @@ import util.JavaVersion;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
 
 public class JavaVersionTest {
     @Test
