722b3d481cb2c7744a0228005258fb493a786091
jinifief <jinified@gmail.com>
2016-03-20 01:55:03 +0800

    Setup gradle dependency and created new base test for TestFX 4

---
+++ src/test/java/guitests/KeyboardShortcutsTest.java
@@ -0,0 +1,211 @@
+package guitests;
+
+import backend.stub.DummyRepoState;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.KeyCode;
+
+import org.junit.Test;
+import org.loadui.testfx.GuiTest;
+
+import ui.UI;
+import ui.components.KeyboardShortcuts;
+import ui.listpanel.ListPanel;
+import util.PlatformEx;
+import util.events.IssueSelectedEventHandler;
+import util.events.PanelClickedEventHandler;
+import util.events.testevents.UIComponentFocusEvent;
+import util.events.testevents.UIComponentFocusEventHandler;
+
+import static org.junit.Assert.assertEquals;
+import static ui.components.KeyboardShortcuts.*;
+
+import static ui.components.KeyboardShortcuts.SWITCH_DEFAULT_REPO;
+
+public class KeyboardShortcutsTest extends NeoUITest {
+
+    private UIComponentFocusEvent.EventType uiComponentFocusEventType;
+    private int selectedIssueId;
+    private int panelIndex;
+
+    @Test
+    public void keyboardShortcutsTest() {
+        UI.events.registerEvent((IssueSelectedEventHandler) e -> selectedIssueId = e.id);
+        UI.events.registerEvent((UIComponentFocusEventHandler) e -> uiComponentFocusEventType = e.eventType);
+        UI.events.registerEvent((PanelClickedEventHandler) e -> panelIndex = e.panelIndex);
+        clearSelectedIssueId();
+        clearUiComponentFocusEventType();
+        clearPanelIndex();
+
+        // maximize
+        assertEquals(false, getStage().getWidth() > 500);
+        press(MAXIMIZE_WINDOW);
+        assertEquals(true, getStage().getWidth() > 500);
+
+        // mid-sized window
+        press(DEFAULT_SIZE_WINDOW);
+        assertEquals(false, getStage().getWidth() > 500);
+
+        // jump from panel focus to first issue
+        // - This is because on startup focus is on panel and not on filter box
+        press(JUMP_TO_FIRST_ISSUE);
+        assertEquals(DummyRepoState.noOfDummyIssues, selectedIssueId);
+        clearSelectedIssueId();
+
+        // jump from issue list to filter box
+        press(JUMP_TO_FILTER_BOX);
+        assertEquals(UIComponentFocusEvent.EventType.FILTER_BOX, uiComponentFocusEventType);
+        clearUiComponentFocusEventType();
+
+        // jump from filter box to first issue
+        // - To ensure shortcut works from filter box, too
+        press(JUMP_TO_FIRST_ISSUE);
+        press(JUMP_TO_FILTER_BOX);
+        assertEquals(DummyRepoState.noOfDummyIssues, selectedIssueId);
+        clearSelectedIssueId();
+
+        // jump to nth issue using number keys 1-9
+        for (int i = 1; i <= 9; i++) {
+            push(KeyCode.ESCAPE);
+            press(JUMP_TO_NTH_ISSUE_KEYS.get(i));
+            PlatformEx.waitOnFxThread();
+            assertEquals(DummyRepoState.noOfDummyIssues - (i - 1), selectedIssueId);
+            clearSelectedIssueId();
+            if (i == 1) {
+                press(JUMP_TO_FILTER_BOX);
+                assertEquals(UIComponentFocusEvent.EventType.FILTER_BOX, uiComponentFocusEventType);
+                clearUiComponentFocusEventType();
+            }
+        }
+
+        // jump to last issue
+        push(KeyCode.END);
+        assertEquals(1, selectedIssueId);
+        clearSelectedIssueId();
+
+        // jump to first issue
+        push(KeyCode.HOME);
+        sleep(1000);
+        assertEquals(DummyRepoState.noOfDummyIssues, selectedIssueId);
+        clearSelectedIssueId();
+
+        push(getKeyCode("DOWN_ISSUE"));
+        assertEquals(DummyRepoState.noOfDummyIssues - 1, selectedIssueId);
+        clearSelectedIssueId();
+        push(getKeyCode("DOWN_ISSUE"));
+        assertEquals(DummyRepoState.noOfDummyIssues - 2, selectedIssueId);
+        clearSelectedIssueId();
+        push(getKeyCode("UP_ISSUE"));
+        assertEquals(DummyRepoState.noOfDummyIssues - 1, selectedIssueId);
+        clearSelectedIssueId();
+
+        press(CREATE_RIGHT_PANEL);
+        press(JUMP_TO_FIRST_ISSUE);
+
+        push(getKeyCode("RIGHT_PANEL"));
+        assertEquals(0, panelIndex);
+        clearPanelIndex();
+        push(getKeyCode("LEFT_PANEL"));
+        assertEquals(1, panelIndex);
+        clearPanelIndex();
+        push(getKeyCode("RIGHT_PANEL"));
+        assertEquals(0, panelIndex);
+        clearPanelIndex();
+        push(getKeyCode("LEFT_PANEL"));
+        assertEquals(1, panelIndex);
+        clearPanelIndex();
+
+        // remove focus from repo selector
+        ComboBox<String> repoSelectorComboBox = GuiTest.find("#repositorySelector");
+        clickOn(repoSelectorComboBox);
+        assertEquals(true, repoSelectorComboBox.isFocused());
+        press(KeyCode.ESCAPE).release(KeyCode.ESCAPE);
+        assertEquals(false, repoSelectorComboBox.isFocused());
+        clearUiComponentFocusEventType();
+
+        // switch default repo tests
+        assertEquals(1, repoSelectorComboBox.getItems().size());
+        // setup - add a new repo
+        doubleClickOn(repoSelectorComboBox);
+        type("dummy1/dummy1");
+        push(KeyCode.ENTER);
+        PlatformEx.waitOnFxThread();
+        assertEquals(2, repoSelectorComboBox.getItems().size());
+        assertEquals("dummy1/dummy1", repoSelectorComboBox.getValue());
+        // test shortcut on repo dropdown
+        doubleClickOn(repoSelectorComboBox);
+        pushKeys(SWITCH_DEFAULT_REPO);
+        // wait for issue 9 to appear then click on it
+        // issue 9 is chosen instead of issue 10
+        // as there is a problem with finding issue 10's node due to it being the first card in the panel
+        waitUntilNodeAppears("#dummy/dummy_col1_" + (DummyRepoState.noOfDummyIssues - 1));
+        assertEquals("dummy/dummy", repoSelectorComboBox.getValue());
+        // test shortcut when focus is on panel
+        clickOn("#dummy/dummy_col1_" + (DummyRepoState.noOfDummyIssues - 1));
+        press(SWITCH_DEFAULT_REPO);
+        PlatformEx.waitOnFxThread();
+        assertEquals("dummy1/dummy1", repoSelectorComboBox.getValue());
+        // test shortcut when focus is on issue list
+        press(JUMP_TO_NTH_ISSUE_KEYS.get(1));
+        press(SWITCH_DEFAULT_REPO);
+        PlatformEx.waitOnFxThread();
+        assertEquals("dummy/dummy", repoSelectorComboBox.getValue());
+
+        // mark as read
+        ListPanel issuePanel = GuiTest.find("#dummy/dummy_col1");
+        // mark as read an issue that has another issue below it
+        push(KeyCode.HOME);
+        // focus should change to the issue below
+        int issueIdBeforeMark = selectedIssueId;
+        int issueIdExpected = issueIdBeforeMark - 1;
+        push(getKeyCode("MARK_AS_READ"));
+        PlatformEx.waitOnFxThread();
+        assertEquals(issueIdExpected, selectedIssueId);
+        push(getKeyCode("UP_ISSUE")); // required since focus has changed to next issue
+        assertEquals(true, issuePanel.getSelectedElement().isPresent());
+        assertEquals(true, issuePanel.getSelectedElement().get().getIssue().isCurrentlyRead());
+
+        // mark as read an issue at the bottom
+        push(KeyCode.END);
+        push(getKeyCode("MARK_AS_READ"));
+        // focus should remain at bottom issue
+        assertEquals(1, selectedIssueId);
+        assertEquals(true, issuePanel.getSelectedElement().isPresent());
+        assertEquals(true, issuePanel.getSelectedElement().get().getIssue().isCurrentlyRead());
+
+        // mark as unread
+        push(getKeyCode("MARK_AS_UNREAD"));
+        assertEquals(true, issuePanel.getSelectedElement().isPresent());
+        assertEquals(false, issuePanel.getSelectedElement().get().getIssue().isCurrentlyRead());
+        clearSelectedIssueId();
+
+        // testing corner case for mark as read where there is only one issue displayed
+        clickOn("#dummy/dummy_col1_filterTextField");
+        type("id:5");
+        push(KeyCode.ENTER);
+        PlatformEx.waitOnFxThread();
+        press(JUMP_TO_FIRST_ISSUE);
+        push(getKeyCode("MARK_AS_READ"));
+        // focus should remain at the only issue shown
+        assertEquals(5, selectedIssueId);
+
+        // minimize window
+        press(MINIMIZE_WINDOW);
+        assertEquals(true, getStage().isIconified());
+    }
+
+    public KeyCode getKeyCode(String shortcut) {
+        return KeyCode.getKeyCode(KeyboardShortcuts.getDefaultKeyboardShortcuts().get(shortcut));
+    }
+
+    public void clearSelectedIssueId() {
+        selectedIssueId = 0;
+    }
+
+    public void clearPanelIndex() {
+        panelIndex = -1;
+    }
+
+    public void clearUiComponentFocusEventType() {
+        uiComponentFocusEventType = UIComponentFocusEvent.EventType.NONE;
+    }
+}
---
+++ src/test/java/guitests/NeoUITest.java
@@ -0,0 +1,450 @@
+package guitests;
+
+import static com.google.common.io.Files.getFileExtension;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.loadui.testfx.GuiTest;
+import org.loadui.testfx.exceptions.NoNodesFoundException;
+import org.loadui.testfx.exceptions.NoNodesVisibleException;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+
+import com.google.common.util.concurrent.SettableFuture;
+
+import backend.interfaces.RepoStore;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBoxBase;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
+import prefs.Preferences;
+import ui.MenuControl;
+import ui.TestController;
+import ui.UI;
+import util.PlatformEx;
+import util.PlatformSpecific;
+
+public class NeoUITest extends FxRobot {
+    
+    private static final Logger logger = LogManager.getLogger(NeoUITest.class.getName());
+    protected static final SettableFuture<Stage> STAGE_FUTURE = SettableFuture.create();
+    private static final Map<Character, KeyCode> specialCharsMap = getSpecialCharsMap();
+
+    // Sets property to run tests headless
+    static {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+        }
+    }
+
+    protected static class TestUI extends UI {
+        public TestUI() {
+            super();
+        }
+
+        @Override
+        public void start(Stage primaryStage) {
+            super.start(primaryStage);
+            STAGE_FUTURE.set(primaryStage);
+        }
+    }
+
+    @BeforeClass
+    public static void setupSpec() {
+        try {
+            FxToolkit.registerPrimaryStage();
+            FxToolkit.toolkitContext().setLaunchTimeoutInMillis(2500);
+            FxToolkit.setupStage(NeoUITest::handleSetupStage);
+        } catch (TimeoutException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void setup() throws Exception {
+        FxToolkit.setupApplication(TestUI.class, "--test=true", "--bypasslogin=true");
+    }
+    
+    public Stage getStage() {
+        return FxToolkit.toolkitContext().getRegisteredStage();
+    }
+
+    private static void handleSetupStage(Stage stage) {
+        // delete test.json if it exists
+        File testConfig = new File(Preferences.DIRECTORY, Preferences.TEST_CONFIG_FILE);
+        if (testConfig.exists() && testConfig.isFile()) {
+            assert testConfig.delete();
+        }
+        clearTestFolder();
+        beforeStageStarts();
+        stage.show();
+        stage.toFront();
+    }
+
+    private static void clearTestFolder() {
+        try {
+            if (Files.exists(Paths.get(RepoStore.TEST_DIRECTORY))) {
+                Files.walk(Paths.get(RepoStore.TEST_DIRECTORY), 1)
+                    .filter(Files::isRegularFile)
+                    .filter(p ->
+                        getFileExtension(String.valueOf(p.getFileName())).equalsIgnoreCase("json") ||
+                            getFileExtension(String.valueOf(p.getFileName())).equalsIgnoreCase("json-err")
+                    )
+                    .forEach(p -> new File(p.toAbsolutePath().toString()).delete());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Map<Character, KeyCode> getSpecialCharsMap() {
+        Map<Character, KeyCode> specialChars = new HashMap<Character, KeyCode>();
+        specialChars.put('~', KeyCode.BACK_QUOTE);
+        specialChars.put('!', KeyCode.DIGIT1);
+        specialChars.put('@', KeyCode.DIGIT2);
+        specialChars.put('#', KeyCode.DIGIT3);
+        specialChars.put('$', KeyCode.DIGIT4);
+        specialChars.put('%', KeyCode.DIGIT5);
+        specialChars.put('^', KeyCode.DIGIT6);
+        specialChars.put('&', KeyCode.DIGIT7);
+        specialChars.put('*', KeyCode.DIGIT8);
+        specialChars.put('(', KeyCode.DIGIT9);
+        specialChars.put(')', KeyCode.DIGIT0);
+        specialChars.put('_', KeyCode.MINUS);
+        specialChars.put('+', KeyCode.EQUALS);
+        specialChars.put('{', KeyCode.OPEN_BRACKET);
+        specialChars.put('}', KeyCode.CLOSE_BRACKET);
+        specialChars.put(':', KeyCode.SEMICOLON);
+        specialChars.put('"', KeyCode.QUOTE);
+        specialChars.put('<', KeyCode.COMMA);
+        specialChars.put('>', KeyCode.PERIOD);
+        specialChars.put('?', KeyCode.SLASH);
+        return Collections.unmodifiableMap(specialChars);
+    }
+
+    private static void beforeStageStarts() {
+        // method to be overridden if anything needs to be done (e.g. to the json) before the stage starts
+    }
+    
+    // ================
+    // Waiting routines
+    // ================
+
+    public void waitUntilNodeAppears(Node node) {
+        GuiTest.waitUntil(node, n -> n.isVisible() && n.getParent() != null);
+    }
+
+    public void waitUntilNodeDisappears(Node node) {
+        GuiTest.waitUntil(node, n -> !n.isVisible() || n.getParent() == null);
+    }
+
+    public void waitUntilNodeAppears(String selector) {
+        awaitCondition(() -> existsQuiet(selector));
+    }
+
+    public void waitUntilNodeDisappears(String selector) {
+        awaitCondition(() -> !existsQuiet(selector));
+    }
+
+    public void waitUntilNodeAppears(Matcher<Object> matcher) {
+        // We use find because there's no `exists` for matchers
+        awaitCondition(() -> findQuiet(matcher).isPresent());
+    }
+
+    public void waitUntilNodeDisappears(Matcher<Object> matcher) {
+        // We use find because there's no `exists` for matchers
+        awaitCondition(() -> !findQuiet(matcher).isPresent());
+    }
+
+    public <T extends Node> void waitUntil(String selector, Predicate<T> condition) {
+        awaitCondition(() -> condition.test(GuiTest.find(selector)));
+    }
+
+    /**
+     * Waits for the result of a function, then asserts that it is equal to some value.
+     */
+    public <T> void waitAndAssertEquals(T expected, Supplier<T> actual) {
+        awaitCondition(() -> expected.equals(actual.get()));
+    }
+
+    public <T> void waitForValue(ComboBoxBase<T> comboBoxBase) {
+        GuiTest.waitUntil(comboBoxBase, c -> c.getValue() != null);
+    }
+
+    public <T extends Node> T findOrWaitFor(String selector) {
+        waitUntilNodeAppears(selector);
+        return GuiTest.find(selector);
+    }
+
+    public <T extends Node> Optional<T> findQuiet(String selectorOrText) {
+        try {
+            return Optional.of(GuiTest.find(selectorOrText));
+        } catch (NoNodesFoundException | NoNodesVisibleException e) {
+            return Optional.empty();
+        }
+    }
+
+    private <T extends Node> Optional<T> findQuiet(Matcher<Object> matcher) {
+        try {
+            return Optional.of(GuiTest.find(matcher));
+        } catch (NoNodesFoundException | NoNodesVisibleException e) {
+            return Optional.empty();
+        }
+
+    }
+
+    public boolean existsQuiet(String selector) {
+        try {
+            return GuiTest.exists(selector);
+        } catch (NoNodesFoundException | NoNodesVisibleException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Allows test threads to busy-wait on some condition.
+     * <p>
+     * Taken from org.loadui.testfx.utils, but modified to synchronise with
+     * the JavaFX Application Thread, with a lower frequency.
+     * <p>
+     * The additional synchronisation prevents bugs where
+     * <p>
+     * awaitCondition(a);
+     * awaitCondition(b);
+     * <p>
+     * sometimes may not be equivalent to
+     * <p>
+     * awaitCondition(a && b);
+     * <p>
+     * The lower frequency is a bit more efficient, since a frequency of 10 ms
+     * just isn't necessary for GUI interactions, and we're bottlenecked by the FX
+     * thread anyway.
+     */
+    public void awaitCondition(Callable<Boolean> condition) {
+        awaitCondition(condition, 5);
+    }
+
+    private void awaitCondition(Callable<Boolean> condition, int timeoutInSeconds) {
+        long timeout = System.currentTimeMillis() + timeoutInSeconds * 1000;
+        try {
+            while (!condition.call()) {
+                Thread.sleep(100);
+                PlatformEx.waitOnFxThread();
+                if (System.currentTimeMillis() > timeout) {
+                    throw new TimeoutException();
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    // ====================
+    // Interaction routines
+    // ====================
+
+    /**
+     * Automate menu traversal by clicking them in order of input parameter
+     *
+     * @param menuNames array of strings of menu item names in sequence of traversal
+     */
+    public void clickMenu(String... menuNames) {
+        for (String menuName : menuNames) {
+            clickOn(menuName);
+        }
+    }
+
+    /**
+     * Sets a text field with given text. Does not simulate clicking and typing 
+     * in the text field.
+     * @param fieldId
+     * @param text
+     */
+    public void setTextField(String fieldId, String text) {
+        waitUntilNodeAppears(fieldId);
+        ((TextField) GuiTest.find(fieldId)).setText(text);
+    }
+
+    /**
+     * Traverses HubTurbo's menu, looking for a chain of nodes with the
+     * given names and triggering their associated action.
+     *
+     * This is a more reliable method of triggering menu items than
+     * {@link #clickMenu}, especially when dealing with nested menu items.
+     * It is a drop-in replacement in most cases.
+     *
+     * Caveats: ensure that adequate synchronisation is used after this method
+     * if it is called from a thread other than the UI thread.
+     *
+     * @param names the chain of menu nodes to visit
+     */
+    public void traverseMenu(String... names) {
+        assert names.length > 0 : "traverseMenu called with no arguments";
+
+        Platform.runLater(() -> {
+            MenuControl root = TestController.getUI().getMenuControl();
+            MenuItem current = root.getMenus().stream()
+                .filter(m -> m.getText().equals(names[0]))
+                .findFirst()
+                .orElseThrow(() ->
+                    new IllegalArgumentException(
+                        String.format("%s is not a valid menu item", names[0])));
+
+            for (int i = 1; i < names.length; i++) {
+                final int j = i;
+                if (!(current instanceof Menu)) {
+                    throw new IllegalArgumentException(
+                        String.format("Menu %s is not as nested as arguments require", names[0]));
+                }
+                current = ((Menu) current).getItems().stream()
+                    .filter(m -> m.getText().equals(names[j]))
+                    .findFirst()
+                    .orElseThrow(() ->
+                        new IllegalArgumentException(
+                            String.format("%s is not a valid menu item", names[j])));
+            }
+
+            current.getOnAction().handle(new ActionEvent());
+        });
+    }
+
+    private List<KeyCode> getKeyCodes(KeyCodeCombination combination) {
+        List<KeyCode> keys = new ArrayList<>();
+        if (combination.getAlt() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(KeyCode.ALT);
+        }
+        if (combination.getShift() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(KeyCode.SHIFT);
+        }
+        if (combination.getMeta() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(KeyCode.META);
+        }
+        if (combination.getControl() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(KeyCode.CONTROL);
+        }
+        if (combination.getShortcut() == KeyCombination.ModifierValue.DOWN) {
+            // Fix bug with internal method not having a proper code for SHORTCUT.
+            // Dispatch manually based on platform.
+            if (PlatformSpecific.isOnMac()) {
+                keys.add(KeyCode.META);
+            } else {
+                keys.add(KeyCode.CONTROL);
+            }
+        }
+        keys.add(combination.getCode());
+        return keys;
+    }
+
+    public void push(KeyCode keyCode, int times) {
+        assert times > 0;
+        for (int i = 0; i < times; i++) {
+            push(keyCode);
+        }
+    }
+
+    public void pushKeys(KeyCodeCombination combination) {
+        pushKeys(getKeyCodes(combination));
+    }
+
+    public void pushKeys(KeyCode... keys) {
+        pushKeys(Arrays.asList(keys));
+    }
+
+    private void pushKeys(List<KeyCode> keys) {
+        keys.forEach(this::press);
+        for (int i = keys.size() - 1; i >= 0; i--) {
+            release(keys.get(i));
+        }
+        PlatformEx.waitOnFxThread();
+    }
+
+    public void press(KeyCodeCombination combination) {
+        press(getKeyCodes(combination));
+    }
+
+    private void press(List<KeyCode> keys) {
+        keys.forEach(this::press);
+        for (int i = keys.size() - 1; i >= 0; i--) {
+            release(keys.get(i));
+        }
+        PlatformEx.waitOnFxThread();
+    }
+    /**
+     * Used to select the whole filter text so that it can be replaced
+     */
+    public void selectAll() {
+        pushKeys(new KeyCodeCombination(KeyCode.A, KeyCombination.SHORTCUT_DOWN));
+    }
+
+    public FxRobot type(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (specialCharsMap.containsKey(text.charAt(i))) {
+                press(KeyCode.SHIFT).press(specialCharsMap.get(text.charAt(i)))
+                    .release(specialCharsMap.get(text.charAt(i))).release(KeyCode.SHIFT);
+
+            } else {
+                String typed = String.valueOf(text.charAt(i)).toUpperCase();
+                if (StringUtils.isNumeric(typed)) typed = "DIGIT" + typed;
+                if (!typed.equals("/")) {
+                    type(KeyCode.valueOf(typed));
+                } else {
+                    type(KeyCode.SLASH);
+                }
+            }
+        }
+        return this;
+    }
+    /**
+     * Performs UI login on the login dialog box.
+     * @param owner The owner of the repo.
+     * @param repoName The repository name
+     * @param username The Github username
+     * @param password The Github password
+     */
+    public void login(String owner, String repoName, String username, String password){
+        selectAll();
+        type(owner).push(KeyCode.TAB);
+        type(repoName).push(KeyCode.TAB);
+        type(username).push(KeyCode.TAB);
+        type(password);
+        clickOn("Sign in");
+    }
+
+    /**
+     * Performs logout from File -> Logout on HubTurbo's pView.
+     */
+    public void logout(){
+        clickMenu("File", "Logout");
+    }
+}
