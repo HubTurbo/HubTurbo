ddd982690f59672e65ce3d2b1ec1680ab1e8816d
You Liang Lim <a0125975@u.nus.edu>
2016-03-21 11:14:33 -0400

    removed blank space at import area.

--- src/main/java/ui/listpanel/ListPanel.java
+++ src/main/java/ui/listpanel/ListPanel.java
@@ -21,7 +21,6 @@ import javafx.scene.layout.Priority;
 import javafx.scene.text.Text;
 import javafx.scene.paint.Color;
 
-
 import javafx.scene.text.TextFlow;
 import org.apache.logging.log4j.Logger;
 
