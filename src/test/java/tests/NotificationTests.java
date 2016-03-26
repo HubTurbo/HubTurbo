ca7d4e69d0a2ae186286a109227545c045d193bb
m133225 <m133225@hotmail.com>
2016-03-26 23:32:06 +0800

    Added test to check that notification is shown when an action succeeds, and is not shown otherwise.

---
+++ src/test/java/tests/NotificationTests.java
@@ -0,0 +1,56 @@
+package tests;
+
+import backend.resource.TurboIssue;
+import javafx.embed.swing.JFXPanel;
+import org.junit.Before;
+import org.junit.Test;
+import ui.NotificationController;
+import ui.components.Notification;
+import undo.UndoController;
+import undo.actions.Action;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.mockito.Mockito.*;
+
+public class NotificationTests {
+    NotificationController notificationController;
+    UndoController undoController;
+    Action<TurboIssue> action;
+    
+    @Before
+    public void initComponents() {
+        // required so notification construction won't fail
+        new JFXPanel();
+
+        action = mock(Action.class);
+        notificationController = mock(NotificationController.class);
+        undoController = new UndoController(notificationController);
+    }
+
+    @Test
+    public void notificationController_actionSuccess_showNotification() {
+        CompletableFuture<Boolean> result = new CompletableFuture<>();
+        result.complete(true);
+
+        TurboIssue issue = mock(TurboIssue.class);
+        doReturn(result).when(action).act(any(TurboIssue.class));
+
+        undoController.addAction(issue, action);
+
+        verify(notificationController, times(1)).showNotification(any(Notification.class));
+    }
+
+    @Test
+    public void notificationController_actionFailed_noNotification() {
+        CompletableFuture<Boolean> result = new CompletableFuture<>();
+        result.complete(false);
+
+        TurboIssue issue = mock(TurboIssue.class);
+        doReturn(result).when(action).act(any(TurboIssue.class));
+
+        undoController.addAction(issue, action);
+
+        verify(notificationController, times(0)).showNotification(any(Notification.class));
+    }
+}
