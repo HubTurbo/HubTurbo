11dbd9215acf58467e5e47844b1cb0345daa8a80
Leow Yijin <leowyijin@gmail.com>
2016-04-05 01:06:04 +0800

    Use UITest waiting methods instead of fixed delay waiting
    Change sleep and assertEquals to awaitCondition
    split tests
    abstracted utility method for subtests
    fixed indentation for checkstyle
    updated manage assignee test case to select issue card first
    ensure issue always selected at start
    merge master and remove defunct tests

--- src/test/java/guitests/ChromeDriverTest.java
+++ src/test/java/guitests/ChromeDriverTest.java
@@ -1,6 +1,8 @@
 package guitests;
 
 import javafx.scene.input.KeyCode;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.testfx.api.FxToolkit;
 
@@ -10,150 +12,187 @@ import util.events.IssueCreatedEvent;
 import util.events.IssueSelectedEvent;
 import util.events.LabelCreatedEvent;
 import util.events.MilestoneCreatedEvent;
-import util.events.testevents.ExecuteScriptEventHandler;
-import util.events.testevents.JumpToNewCommentBoxEventHandler;
-import util.events.testevents.NavigateToPageEventHandler;
+import util.events.testevents.*;
 
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import java.util.concurrent.TimeoutException;
 
 public class ChromeDriverTest extends UITest {
 
-    private static final int EVENT_DELAY = 2000;
-    private String url;
-    private String script;
+    private final List<String> urlsNavigated = new ArrayList<>();
+    private final List<String> scriptsExecuted = new ArrayList<>();
+    private final List<String> keysSentToBrowser = new ArrayList<>();
+    private boolean hasJumpedToComment = false;
 
-    private boolean jumpToComment = false; // NOPMD
+    private final NavigateToPageEventHandler navToPageHandler = e -> urlsNavigated.add(e.url);
+    private final ExecuteScriptEventHandler execScriptHandler = e -> scriptsExecuted.add(e.script);
+    private final JumpToNewCommentBoxEventHandler jumpCommentHandler = e -> hasJumpedToComment = true;
+
+    @Before
+    public void cleanAndRegisterHandlers() {
+        urlsNavigated.clear();
+        scriptsExecuted.clear();
+        keysSentToBrowser.clear();
+        hasJumpedToComment = false;
+        UI.events.registerEvent(navToPageHandler);
+        UI.events.registerEvent(execScriptHandler);
+        UI.events.registerEvent(jumpCommentHandler);
+        clickIssue(0, 9);
+    }
+
+    @After
+    public void unregisterHandlers() {
+        UI.events.unregisterEvent(navToPageHandler);
+        UI.events.unregisterEvent(execScriptHandler);
+        UI.events.unregisterEvent(jumpCommentHandler);
+    }
 
     @Override
     public void setup() throws TimeoutException {
         FxToolkit.setupApplication(
             TestUI.class, "--test=true", "--bypasslogin=true", "--testchromedriver=true");
     }
+    /**
+     * Performs the specified action and waits for a specific response to occur.
+     *
+     * @param expected expected response to the action.
+     * @param action the action to take to trigger the desired response.
+     * @param responses aggregator of responses to the action.
+     */
+    private <T> void actAndAwaitResponse(T expected, Runnable action, Collection<T> responses) {
+        responses.clear();
+        action.run();
+        awaitCondition(() -> responses.contains(expected));
+        responses.clear();
+    }
 
     @Test
-    public void chromeDriverStubTest() {
-        clearUrl();
-        clearScript();
-
-        UI.events.registerEvent((NavigateToPageEventHandler) e -> url = e.url);
-        UI.events.registerEvent((ExecuteScriptEventHandler) e -> script = e.script);
-        UI.events.registerEvent((JumpToNewCommentBoxEventHandler) e -> jumpToComment = true);
-
-        UI.events.triggerEvent(new IssueSelectedEvent("dummy/dummy", 1, 0, false));
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForIssue("dummy/dummy", 1), url);
-        clearUrl();
-        UI.events.triggerEvent(new IssueCreatedEvent());
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForNewIssue("dummy/dummy"), url);
-        clearUrl();
-        UI.events.triggerEvent(new LabelCreatedEvent());
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForNewLabel("dummy/dummy"), url);
-        clearUrl();
-        UI.events.triggerEvent(new MilestoneCreatedEvent());
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForNewMilestone("dummy/dummy"), url);
-        clearUrl();
-
-        clickIssue(0, 9);
-        sleep(EVENT_DELAY);
-        clearUrl();
+    public void navigateToPage() {
+        actAndAwaitResponse(
+            GitHubURL.getPathForIssue("dummy/dummy", 1),
+            () -> UI.events.triggerEvent(new IssueSelectedEvent("dummy/dummy", 1, 0, false)),
+            urlsNavigated
+        );
+        actAndAwaitResponse(
+            GitHubURL.getPathForNewIssue("dummy/dummy"),
+            () -> UI.events.triggerEvent(new IssueCreatedEvent()),
+            urlsNavigated
+        );
+        actAndAwaitResponse(
+            GitHubURL.getPathForNewLabel("dummy/dummy"),
+            () -> UI.events.triggerEvent(new LabelCreatedEvent()),
+            urlsNavigated
+        );
+        actAndAwaitResponse(
+            GitHubURL.getPathForNewMilestone("dummy/dummy"),
+            () -> UI.events.triggerEvent(new MilestoneCreatedEvent()),
+            urlsNavigated
+        );
 
         // show docs
-        push(KeyCode.F1);
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.DOCS_PAGE, url);
-        clearUrl();
-        push(KeyCode.G).push(KeyCode.H);
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.DOCS_PAGE, url);
-        clearUrl();
-
-        // scroll to top
-        push(KeyCode.I);
-        sleep(EVENT_DELAY);
-        assertEquals("window.scrollTo(0, 0)", script);
-        clearScript();
-
-        // scroll to bottom
-        push(KeyCode.N);
-        sleep(EVENT_DELAY);
-        assertEquals("window.scrollTo(0, document.body.scrollHeight)", script);
-        clearScript();
-
-        // scroll up
-        push(KeyCode.J);
-        sleep(EVENT_DELAY);
-        assertEquals("window.scrollBy(0, -100)", script);
-        clearScript();
-
-        // scroll down
-        push(KeyCode.K);
-        sleep(EVENT_DELAY);
-        assertEquals("window.scrollBy(0, 100)", script);
-        clearScript();
-
+        actAndAwaitResponse(
+            GitHubURL.DOCS_PAGE,
+            () -> push(KeyCode.F1),
+            urlsNavigated
+        );
+        actAndAwaitResponse(
+            GitHubURL.DOCS_PAGE,
+            () -> {
+                push(KeyCode.G);
+                push(KeyCode.H);
+            },
+            urlsNavigated
+        );
+        actAndAwaitResponse(
+            GitHubURL.DOCS_PAGE,
+            () -> {
+                clickOn("View");
+                clickOn("Documentation");
+            },
+            urlsNavigated
+        );
         // go to labels page
-        push(KeyCode.G).push(KeyCode.L);
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForNewLabel("dummy/dummy"), url);
-        clearUrl();
+        actAndAwaitResponse(
+            GitHubURL.getPathForNewLabel("dummy/dummy"),
+            () -> push(KeyCode.G).push(KeyCode.L),
+            urlsNavigated
+        );
 
         // go to issues page
-        push(KeyCode.G).push(KeyCode.I);
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForAllIssues("dummy/dummy"), url);
-        clearUrl();
+        actAndAwaitResponse(
+            GitHubURL.getPathForAllIssues("dummy/dummy"),
+            () -> push(KeyCode.G).push(KeyCode.I),
+            urlsNavigated
+        );
 
         // go to milestones page
-        push(KeyCode.G).push(KeyCode.M);
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForMilestones("dummy/dummy"), url);
-        clearUrl();
+        actAndAwaitResponse(
+            GitHubURL.getPathForMilestones("dummy/dummy"),
+            () -> push(KeyCode.G).push(KeyCode.M),
+            urlsNavigated
+        );
 
         // go to pull requests page
-        push(KeyCode.G).push(KeyCode.P);
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForPullRequests("dummy/dummy"), url);
-        clearUrl();
+        actAndAwaitResponse(
+            GitHubURL.getPathForPullRequests("dummy/dummy"),
+            () -> push(KeyCode.G).push(KeyCode.P),
+            urlsNavigated
+        );
 
         // go to developers page
-        push(KeyCode.G).push(KeyCode.D);
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.getPathForContributors("dummy/dummy"), url);
-        clearUrl();
+        actAndAwaitResponse(
+            GitHubURL.getPathForContributors("dummy/dummy"),
+            () -> push(KeyCode.G).push(KeyCode.D),
+            urlsNavigated
+        );
 
         // go to keyboard shortcuts page
-        push(KeyCode.G).push(KeyCode.K);
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.KEYBOARD_SHORTCUTS_PAGE, url);
-        clearUrl();
+        actAndAwaitResponse(
+            GitHubURL.KEYBOARD_SHORTCUTS_PAGE,
+            () -> push(KeyCode.G).push(KeyCode.K),
+            urlsNavigated
+        );
+    }
 
+    @Test
+    public void executeScripts() {
+        // scroll to top
+        actAndAwaitResponse(
+            "window.scrollTo(0, 0)",
+            () -> push(KeyCode.I),
+            scriptsExecuted)
+        ;
+
+        // scroll to bottom
+        actAndAwaitResponse(
+            "window.scrollTo(0, document.body.scrollHeight)",
+            () -> push(KeyCode.N),
+            scriptsExecuted
+        );
+
+        // scroll up
+        actAndAwaitResponse(
+            "window.scrollBy(0, -100)",
+            () -> push(KeyCode.J),
+            scriptsExecuted
+        );
+
+        // scroll down
+        actAndAwaitResponse(
+            "window.scrollBy(0, 100)",
+            () -> push(KeyCode.K),
+            scriptsExecuted
+        );
+    }
+
+    @Test
+    public void jumpToNewCommentBox() {
         // jump to comments
-        push(KeyCode.R);
-        sleep(EVENT_DELAY);
-        assertEquals(true, jumpToComment);
-        jumpToComment = false;
-
-        clickOn("View");
-        clickOn("Documentation");
-        sleep(EVENT_DELAY);
-        assertEquals(GitHubURL.DOCS_PAGE, url);
-        clearUrl();
-
-        clickOn("File");
-        clickOn("Logout");
-    }
-
-    public void clearUrl() {
-        url = "";
-    }
-
-    public void clearScript() {
-        script = "";
+        push(KeyCode.R, 1);
+        awaitCondition(() -> hasJumpedToComment);
     }
 
 }
